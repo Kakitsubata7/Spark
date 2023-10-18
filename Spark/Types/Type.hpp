4d6b56544fd2ae99b72456fe180a04c163d021e2
Dongxin Tian <70252050+Dongxin-Tian@users.noreply.github.com>
2023-10-18 14:03:37 -0400

    Type type

---
+++ Spark/Types/Type.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <cstdint>
+
+namespace Spark {
+
+    enum class Type : uint64_t {
+        None = 0x00,
+        Type = 0x01,
+        Integer = 0x02,
+        Float = 0x03,
+        Boolean = 0x04,
+        String = 0x05,
+        Array = 0x06,
+        Table = 0x07,
+        Object = 0x08,
+        Coroutine = 0x09,
+        Thread = 0x0A
+    };
+
+    static_assert(sizeof(Type) == 8, R"(The size of type "Spark::Type" should to be exactly 8 bytes.)");
+
+} // Spark
