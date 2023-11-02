b8542f92042731ffc165ca88df7dace44f33ff1b
Dongxin Tian <kakitsubata0110@gmail.com>
2023-11-02 18:11:25 -0400

    Implemented Spark::Integer

--- Spark/Types/Integer.hpp
+++ Spark/Types/Integer.hpp
@@ -1,3 +1,107 @@
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "google-explicit-constructor"
 #pragma once
 
-namespace 
+#include <cstdint>
+#include <ostream>
+
+namespace Spark {
+
+    struct Integer {
+
+        /* ===== Data ===== */
+
+    private:
+        int64_t value;
+
+
+
+        /* ===== Constructors ===== */
+
+    public:
+        constexpr Integer() : value(0) { }
+        constexpr Integer(char value) : value(static_cast<int64_t>(value)) { }
+        constexpr Integer(unsigned char value) : value(static_cast<int64_t>(value)) { }
+        constexpr Integer(short value) : value(static_cast<int64_t>(value)) { }
+        constexpr Integer(unsigned short value) : value(static_cast<int64_t>(value)) { }
+        constexpr Integer(int value) : value(static_cast<int64_t>(value)) { }
+        constexpr Integer(unsigned int value) : value(static_cast<int64_t>(value)) { }
+        constexpr Integer(long value) : value(static_cast<int64_t>(value)) { }
+        constexpr Integer(unsigned long value) : value(static_cast<int64_t>(value)) { }
+        constexpr Integer(long long value) : value(static_cast<int64_t>(value)) { }
+        constexpr Integer(unsigned long long value) : value(static_cast<int64_t>(value)) { }
+        constexpr Integer(float value) : value(static_cast<int64_t>(value)) { }
+        constexpr Integer(double value) : value(static_cast<int64_t>(value)) { }
+        constexpr Integer(long double value) : value(static_cast<int64_t>(value)) { }
+
+
+
+        /* ===== Operators ===== */
+
+    public:
+        constexpr Integer operator+(const Integer other) const {
+            return {value + other.value};
+        }
+
+        constexpr Integer& operator++() {
+            value++;
+            return *this;
+        }
+
+        constexpr Integer& operator--() {
+            value--;
+            return *this;
+        }
+
+        constexpr const Integer operator++(int) {
+            Integer temp = *this;
+            value++;
+            return temp;
+        }
+
+        constexpr const Integer operator--(int) {
+            Integer temp = *this;
+            value--;
+            return temp;
+        }
+
+        constexpr Integer operator-(const Integer other) const {
+            return {value - other.value};
+        }
+
+        constexpr Integer operator*(const Integer other) const {
+            return {value * other.value};
+        }
+
+        constexpr Integer operator/(const Integer other) const {
+            if (other.value == 0)
+                throw std::runtime_error("Division by zero.");
+            return {value / other.value};
+        }
+
+        constexpr Integer operator%(const Integer other) const {
+            return {value % other.value};
+        }
+
+        constexpr Integer operator&(const Integer other) const {
+            return {value & other.value};
+        }
+
+        constexpr Integer operator|(const Integer other) const {
+            return {value | other.value};
+        }
+
+        constexpr Integer operator^(const Integer other) const {
+            return {value ^ other.value};
+        }
+
+        friend std::ostream& operator<<(std::ostream& os, const Integer integer) {
+            os << integer.value;
+            return os;
+        }
+
+    };
+
+} // Spark
+
+#pragma clang diagnostic pop
