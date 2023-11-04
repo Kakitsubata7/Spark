8ada42bdf51be47f8a224eb96936f058efba7f95
Dongxin Tian <kakitsubata0110@gmail.com>
2023-11-03 22:14:15 -0400

    Update Float.hpp

--- Spark/Types/Float.hpp
+++ Spark/Types/Float.hpp
@@ -9,6 +9,36 @@ namespace Spark {
     private:
         double value;
 
+
+
+        /* ===== Constructors ===== */
+
+    public:
+        constexpr Float() : value(0.0) { }
+        constexpr Float(char value) : value(static_cast<double>(value)) { }
+        constexpr Float(unsigned char value) : value(static_cast<double>(value)) { }
+        constexpr Float(short value) : value(static_cast<double>(value)) { }
+        constexpr Float(unsigned short value) : value(static_cast<double>(value)) { }
+        constexpr Float(int value) : value(static_cast<double>(value)) { }
+        constexpr Float(unsigned int value) : value(static_cast<double>(value)) { }
+        constexpr Float(long value) : value(static_cast<double>(value)) { }
+        constexpr Float(unsigned long value) : value(static_cast<double>(value)) { }
+        constexpr Float(long long value) : value(static_cast<double>(value)) { }
+        constexpr Float(unsigned long long value) : value(static_cast<double>(value)) { }
+        constexpr Float(float value) : value(static_cast<double>(value)) { }
+        constexpr Float(double value) : value(static_cast<double>(value)) { }
+        constexpr Float(long double value) : value(static_cast<double>(value)) { }
+
+
+
+        /* ===== Operators ===== */
+
+    public:
+        friend std::ostream& operator<<(std::ostream& os, const Float f) {
+            os << f.value;
+            return os;
+        }
+
     };
 
 } // Spark
