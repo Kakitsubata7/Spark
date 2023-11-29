ee9fdb09158cafae5e74c1ba88a8b3d087260d7e
Dongxin Tian <70252050+Dongxin-Tian@users.noreply.github.com>
2023-11-29 13:24:26 -0500

    Added operators

--- Spark/Types/Boolean.hpp
+++ Spark/Types/Boolean.hpp
@@ -18,6 +18,21 @@ private:
 public:
     constexpr Boolean() : value(0) { }
     constexpr Boolean(const bool value) : value(value ? 1 : 0) { }
+    explicit constexpr Boolean(const uint8_t value) : value(value) { }
+
+
+
+    /* ===== Conversion Operators ===== */
+
+public:
+    constexpr operator bool() const { return value != 0; }
+
+
+
+    /* ===== Operators ===== */
+
+public:
+    constexpr Boolean operator !() const { return !value; }
 
 
 
