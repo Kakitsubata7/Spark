a21d620fd2e8dabecf505ec727fdf39148d67df4
Dongxin Tian <70252050+Dongxin-Tian@users.noreply.github.com>
2023-11-29 14:52:06 -0500

    Create Value.hpp

---
+++ Spark/Types/Value.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "Boolean.hpp"
+#include "Float.hpp"
+#include "Integer.hpp"
+#include "Type.hpp"
+
+namespace Spark {
+
+class Value {
+
+    /* ===== Data ===== */
+
+private:
+    Type type;
+    Boolean isConstant;
+    union {
+        Type typeValue;
+        Integer integerValue;
+        Float floatValue;
+        Boolean booleanValue;
+        void* pointerValue;
+    };
+
+
+
+    /* ===== Constructors ===== */
+
+public:
+    Value() : type(Type::TypeEnum::None), isConstant(false) {
+        integerValue = 0;
+        pointerValue = nullptr;
+    }
+
+};
+
+} // Spark
