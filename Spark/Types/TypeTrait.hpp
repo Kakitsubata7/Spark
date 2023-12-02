a59d9546ffdc50646526ab24a37aa7efc4687934
Dongxin Tian <kakitsubata0110@gmail.com>
2023-12-02 17:36:20 -0500

    Added isSparkType overloadings for 'Type' and 'Boolean'

--- Spark/Types/TypeTrait.hpp
+++ Spark/Types/TypeTrait.hpp
@@ -8,10 +8,16 @@ namespace Spark {
     inline constexpr bool isSparkType = false;
 
     template <>
+    inline constexpr bool isSparkType<Type> = true;
+
+    template <>
     inline constexpr bool isSparkType<Integer> = true;
 
     template <>
     inline constexpr bool isSparkType<Float> = true;
+
+    template <>
+    inline constexpr bool isSparkType<Boolean> = true;
 
     template <>
     inline constexpr bool isSparkType<String> = true;
