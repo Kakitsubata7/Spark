2d64f5e2f83f084e8cf859f9f08129e87fcc51ba
Dongxin Tian <kakitsubata0110@gmail.com>
2023-12-06 21:03:34 -0500

    Create Type.hpp

---
+++ Spark/Type.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <cstdint>
+
+namespace Spark {
+
+    enum class Type : uint8_t {
+        Nil,
+        Integer,
+        Float,
+        Boolean,
+        Pointer,
+        Type,
+        String,
+        Array,
+        Set,
+        Map,
+        Object,
+        Function
+    };
+
+} // Spark
