85fdc9842ded8855b2c2e7097c56d91f798c23bd
Dongxin Tian <kakitsubata0110@gmail.com>
2023-12-07 13:45:43 -0500

    Used type traits to implement factory methods

--- Spark/Value.hpp
+++ Spark/Value.hpp
@@ -4,6 +4,7 @@
 #include <iomanip>
 #include <ostream>
 #include <string>
+#include <type_traits>
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
@@ -51,137 +52,181 @@ public:
     /* ===== Constructor ===== */
 
 public:
-    constexpr Value() : _type(Type::Nil), _isConstant(false), pointerValue(nullptr) { }
+    constexpr Value() : Value(false) { }
+
+private:
+    explicit constexpr Value(bool isConstant) : _type(Type::Nil), _isConstant(isConstant), pointerValue(nullptr) { }
+
+
+
+    /* ===== Type Traits ===== */
+
+private:
+    template <typename T>
+    inline static constexpr bool is_integer_v = std::disjunction_v<std::is_same<short, T>,
+                                                                   std::is_same<unsigned short, T>,
+                                                                   std::is_same<int, T>,
+                                                                   std::is_same<unsigned int, T>,
+                                                                   std::is_same<long, T>,
+                                                                   std::is_same<unsigned long, T>,
+                                                                   std::is_same<long long, T>,
+                                                                   std::is_same<unsigned long long, T>>;
+
+    template <typename T>
+    inline static constexpr bool is_float_v = std::disjunction_v<std::is_same<float, T>,
+                                                                 std::is_same<double, T>,
+                                                                 std::is_same<long double, T>>;
+
+    template <typename T>
+    inline static constexpr bool is_boolean_v = std::disjunction_v<std::is_same<bool, T>,
+                                                                   std::is_same<Bool8, T>>;
+
+    template <typename T>
+    inline static constexpr bool is_pointer_v = std::disjunction_v<std::is_same<void*, T>,
+                                                                   std::is_same<std::nullptr_t, T>>;
+
+    template <typename T>
+    inline static constexpr bool is_type_v = std::is_same_v<Type, T>;
+
+    template <typename T>
+    using IntegerValue = typename std::enable_if_t<is_integer_v<T>, Value>;
+
+    template <typename T>
+    using FloatValue = typename std::enable_if_t<is_float_v<T>, Value>;
+
+    template <typename T>
+    using BooleanValue = typename std::enable_if_t<is_boolean_v<T>, Value>;
+
+    template <typename T>
+    using PointerValue = typename std::enable_if_t<is_pointer_v<T>, Value>;
+
+    template <typename T>
+    using TypeValue = typename std::enable_if_t<is_type_v<T>, Value>;
 
 
 
     /* ===== Factory Methods ===== */
 
-public:
+private:
     template <typename T>
     [[nodiscard]]
-    static typename std::enable_if_t<std::is_same_v<short, T> ||
-                            std::is_same_v<unsigned short, T> ||
-                            std::is_same_v<int, T> ||
-                            std::is_same_v<unsigned int, T> ||
-                            std::is_same_v<long, T> ||
-                            std::is_same_v<unsigned long, T> ||
-                            std::is_same_v<long long, T> ||
-                            std::is_same_v<unsigned long long, T>, Value>
-    make() {
+    inline static IntegerValue<T> construct(T value, bool isConstant) {
         Value result;
         result._type = Type::Integer;
-        result._isConstant = false;
-        result.integerValue = 0;
-        return result;
-    }
-
-    template <typename T>
-    [[nodiscard]]
-    static typename std::enable_if_t<std::is_same_v<short, T> ||
-                                     std::is_same_v<unsigned short, T> ||
-                                     std::is_same_v<int, T> ||
-                                     std::is_same_v<unsigned int, T> ||
-                                     std::is_same_v<long, T> ||
-                                     std::is_same_v<unsigned long, T> ||
-                                     std::is_same_v<long long, T> ||
-                                     std::is_same_v<unsigned long long, T>, Value>
-    make(T value) {
-        Value result;
-        result._type = Type::Integer;
-        result._isConstant = false;
+        result._isConstant = isConstant;
         result.integerValue = static_cast<Int64>(value);
         return result;
     }
 
     template <typename T>
     [[nodiscard]]
-    static const typename std::enable_if_t<std::is_same_v<short, T> ||
-                                           std::is_same_v<unsigned short, T> ||
-                                           std::is_same_v<int, T> ||
-                                           std::is_same_v<unsigned int, T> ||
-                                           std::is_same_v<long, T> ||
-                                           std::is_same_v<unsigned long, T> ||
-                                           std::is_same_v<long long, T> ||
-                                           std::is_same_v<unsigned long long, T>, Value>
-    makeConstant(T value) {
-        Value result;
-        result._type = Type::Integer;
-        result._isConstant = true;
-        result.integerValue = static_cast<Int64>(value);
-        return result;
-    }
-
-    template <typename T>
-    [[nodiscard]]
-    static typename std::enable_if_t<std::is_same_v<float, T> ||
-                            std::is_same_v<double, T> ||
-                            std::is_same_v<long double, T>, Value>
-    make() {
+    inline static FloatValue<T> construct(T value, bool isConstant) {
         Value result;
         result._type = Type::Float;
-        result._isConstant = false;
-        result.floatValue = 0.0;
-        return result;
-    }
-
-    template <typename T>
-    [[nodiscard]]
-    static typename std::enable_if_t<std::is_same_v<float, T> ||
-                                     std::is_same_v<double, T> ||
-                                     std::is_same_v<long double, T>, Value>
-    make(T value) {
-        Value result;
-        result._type = Type::Float;
-        result._isConstant = false;
+        result._isConstant = isConstant;
         result.floatValue = static_cast<Float64>(value);
         return result;
     }
 
     template <typename T>
     [[nodiscard]]
-    static typename std::enable_if_t<std::is_same_v<void*, T> ||
-                                     std::is_same_v<nullptr_t, T>, Value>
-    make() {
+    inline static BooleanValue<T> construct(T value, bool isConstant) {
         Value result;
-        result._type = Type::Pointer;
-        result._isConstant = false;
-        result.pointerValue = nullptr;
+        result._type = Type::Boolean;
+        result._isConstant = isConstant;
+        result.booleanValue = static_cast<Bool8>(value);
         return result;
     }
 
     template <typename T>
     [[nodiscard]]
-    static typename std::enable_if_t<std::is_same_v<void*, T> ||
-                                     std::is_same_v<nullptr_t, T>, Value>
-    make(T value) {
+    inline static PointerValue<T> construct(T value, bool isConstant) {
         Value result;
         result._type = Type::Pointer;
-        result._isConstant = false;
+        result._isConstant = isConstant;
         result.pointerValue = static_cast<void*>(value);
         return result;
     }
 
     template <typename T>
     [[nodiscard]]
-    static typename std::enable_if_t<std::is_same_v<bool, T>, Value>
-    make() {
+    inline static TypeValue<T> construct(T value, bool isConstant) {
         Value result;
-        result._type = Type::Boolean;
-        result._isConstant = false;
-        result.booleanValue = false;
+        result._type = Type::Type;
+        result._isConstant = isConstant;
+        result.typeValue = static_cast<Type>(value);
         return result;
+    }
+
+public:
+    [[nodiscard]]
+    constexpr static Value makeNil() {
+        return Value(false);
+    }
+
+    [[nodiscard]]
+    constexpr static Value makeConstantNil() {
+        return Value(true);
     }
 
     template <typename T>
     [[nodiscard]]
-    static typename std::enable_if_t<std::is_same_v<bool, T>, Value>
-    make(bool value) {
-        Value result;
-        result._type = Type::Boolean;
-        result._isConstant = false;
-        result.booleanValue = value;
-        return result;
+    static IntegerValue<T> make(T value = 0) {
+        return construct<T>(value, false);
+    }
+
+    template <typename T>
+    [[nodiscard]]
+    static const IntegerValue<T> makeConstant(T value = 0) {
+        return construct<T>(value, true);
+    }
+
+    template <typename T>
+    [[nodiscard]]
+    static FloatValue<T> make(T value = 0.0) {
+        return construct<T>(value, false);
+    }
+
+    template <typename T>
+    [[nodiscard]]
+    static const FloatValue<T> makeConstant(T value = 0.0) {
+        return construct<T>(value, true);
+    }
+
+    template <typename T>
+    [[nodiscard]]
+    static BooleanValue<T> make(T value = false) {
+        return construct<T>(value, false);
+    }
+
+    template <typename T>
+    [[nodiscard]]
+    static const BooleanValue<T> makeConstant(T value = false) {
+        return construct<T>(value, true);
+    }
+
+    template <typename T>
+    [[nodiscard]]
+    static PointerValue<T> make(T value = nullptr) {
+        return construct<T>(value, false);
+    }
+
+    template <typename T>
+    [[nodiscard]]
+    static const PointerValue<T> makeConstant(T value = nullptr) {
+        return construct<T>(value, true);
+    }
+
+    template <typename T>
+    [[nodiscard]]
+    static TypeValue<T> make(T value = nullptr) {
+        return construct<T>(value, false);
+    }
+
+    template <typename T>
+    [[nodiscard]]
+    static const TypeValue<T> makeConstant(T value = nullptr) {
+        return construct<T>(value, true);
     }
 
 
@@ -215,6 +260,35 @@ public:
                    << reinterpret_cast<uintptr_t>(value.pointerValue)
                    << std::dec;
             }
+                break;
+
+            case Type::Type:
+                os << value.typeValue;
+                break;
+
+            // TODO: Implement
+            case Type::String:
+                break;
+
+            case Type::Array:
+                break;
+
+            case Type::Set:
+                break;
+
+            case Type::Map:
+                break;
+
+            case Type::Object:
+                break;
+
+            case Type::Function:
+                break;
+
+            case Type::Thread:
+                break;
+
+            case Type::Promise:
                 break;
         }
         return os;
