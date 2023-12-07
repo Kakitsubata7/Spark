4e27ee17b597d75e41e47c77a6ef07cc6ee2d5ad
Dongxin Tian <kakitsubata0110@gmail.com>
2023-12-07 02:55:44 -0500

    Update Value.hpp

--- Spark/Value.hpp
+++ Spark/Value.hpp
@@ -6,7 +6,10 @@
 #include <unordered_set>
 #include <vector>
 
+#include "Bool8.hpp"
+#include "Float64.hpp"
 #include "GC/GCPtr.hpp"
+#include "Int64.hpp"
 #include "Type.hpp"
 
 namespace Spark {
@@ -16,12 +19,13 @@ class Value {
     /* ===== Data ===== */
 
 private:
-    Type type;
+    Type _type;
+    Bool8 _isConstant;
 
     union {
-        int64_t integerValue;
-        double floatValue;
-        bool booleanValue;
+        Int64 integerValue;
+        Float64 floatValue;
+        Bool8 booleanValue;
         void* pointerValue;
         Type typeValue;
         GCPtr<std::string> stringPtr;
@@ -29,7 +33,27 @@ private:
         GCPtr<std::unordered_set<Value>> setPtr;
         GCPtr<std::unordered_map<Value, Value>> mapPtr;
         GCPtr<std::unordered_map<std::string, Value>> objectPtr;
+        GCPtr<void*> threadPtr;
+        GCPtr<void*> promisePtr;
     };
+
+public:
+    [[nodiscard]]
+    constexpr Type type() const { return _type; }
+
+    [[nodiscard]]
+    constexpr Bool8 isConstant() const { return _isConstant; }
+
+
+
+    /* ===== Constructor ===== */
+
+private:
+    constexpr Value() : _type(Type::Nil), _isConstant(false), pointerValue(nullptr) { }
+
+
+
+    /* ===== Factory Methods ===== */
 
 };
 
