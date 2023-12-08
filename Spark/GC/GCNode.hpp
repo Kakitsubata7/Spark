a8dbbd904ad544b00d19ade10011a93f9ab8cb6e
Dongxin Tian <kakitsubata0110@gmail.com>
2023-12-07 21:46:12 -0500

    Worked on GC stuff

---
+++ Spark/GC/GCNode.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace Spark {
+
+template <typename T>
+class GCNode {
+
+};
+
+} // Spark
--- Spark/GC/GCPtr.hpp
+++ Spark/GC/GCPtr.hpp
@@ -1,10 +1,36 @@
 #pragma once
 
+#include "GCNode.hpp"
+
 namespace Spark {
 
-    template <typename T>
-    class GCPtr {
+template <typename T>
+class GCPtr {
 
-    };
+    /* ===== Data ===== */
+
+private:
+    class {
+        size_t count = 0;
+        GCNode<T>* node = nullptr;
+    }* data;
+
+
+
+//    /* ===== Constructor ===== */
+//
+//private:
+//
+//
+//
+//    /* ===== Factory Methods ===== */
+//
+//public:
+//    template <typename... Args>
+//    static GCPtr<T> make(Args&&... args) {
+//        return GCPtr<T>(new T(std::forward<Args>(args)...));
+//    }
+
+};
 
 } // Spark
