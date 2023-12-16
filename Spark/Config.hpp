08812e0b7067423644bdfafb1e2013b224d1ea82
Dongxin Tian <kakitsubata0110@gmail.com>
2023-12-16 02:16:47 -0500

    Stack constants

---
+++ Spark/Config.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <cstddef>
+
+namespace Spark {
+
+    /* ===== Stack ===== */
+
+    constexpr size_t DEFAULT_STACK_CAPACITY = 4096;
+    constexpr size_t DEFAULT_MAX_STACK_CAPACITY = 2'097'152;
+
+} // Spark
