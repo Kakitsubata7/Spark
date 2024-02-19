898863d27e04588610cc9c36bfb745907f255ee1
Dongxin Tian <70252050+Dongxin-Tian@users.noreply.github.com>
2024-02-19 11:44:24 -0500

    Switched to ANSI C

--- Spark/Float64.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-namespace Spark {
-
-    using Float64 = double;
-
-} // Spark
--- Spark/Int64.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <cstdint>
-
-namespace Spark {
-
-    using Int64 = int64_t;
-
-} // Spark
