be159bb5b49a0df882a4c661c7de0d4ecd3aba52
Dongxin Tian <70252050+Dongxin-Tian@users.noreply.github.com>
2024-02-19 12:03:02 -0500

    Created type 'spk_float_t'

---
+++ spark/float.h
@@ -0,0 +1,11 @@
+#ifndef SPK_FLOAT_H
+#define SPK_FLOAT_H
+
+#include <float.h>
+
+typedef double spk_float_t;
+
+#define SPK_FLOAT_MAX DBL_MAX
+#define SPK_FLOAT_MIN DBL_MIN
+
+#endif /* SPK_FLOAT_H */
