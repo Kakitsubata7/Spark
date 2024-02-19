133ec7ab8563cbc879da7e8676d1540e4a238299
Dongxin Tian <70252050+Dongxin-Tian@users.noreply.github.com>
2024-02-19 11:53:25 -0500

    Created and tested type 'spk_bool8'

--- main.c
+++ main.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "spark/spk_bool8.h"
+
 int main(void) {
 
-    printf("Hello world!"); /* Comment */
+    spk_bool8 bool = SPK_TRUE;
+
+    if (bool)
+        printf("Hello world!"); /* Comment */
 
     return EXIT_SUCCESS;
 }
---
+++ spark/spk_bool8.h
@@ -0,0 +1,11 @@
+#ifndef SPK_BOOL8_H
+#define SPK_BOOL8_H
+
+#include <stdint.h>
+
+typedef uint8_t spk_bool8;
+
+#define SPK_TRUE 1
+#define SPK_FALSE 0
+
+#endif /* SPK_BOOL8_H */
