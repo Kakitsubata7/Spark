533ae46cd7d18b89c154fa7f8ea7e6750201e327
Dongxin Tian <70252050+Dongxin-Tian@users.noreply.github.com>
2024-02-19 12:00:04 -0500

    Renamed 'spk_bool8_t' to 'spk_bool_r'

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "spark/spk_bool8.h"
+#include "spark/bool.h"
 
 int main(void) {
 
-    spk_bool8 bool = SPK_TRUE;
+    spk_bool8_t bool = SPK_TRUE;
 
     if (bool)
         printf("Hello world!"); /* Comment */
