2eccedc817dfa024570353270071b662c50a437b
Dongxin Tian <70252050+Dongxin-Tian@users.noreply.github.com>
2024-03-23 14:11:30 -0400

    Implemented 'UnreferenceOperation'

---
+++ Spark/GC/UnreferenceOperation.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <list>
+
+#include "GCNode.hpp"
+#include "GCOperation.hpp"
+
+namespace Spark {
+
+class UnreferenceOperation : public GCOperation {
+
+public:
+    UnreferenceOperation(GCNode* unreferencer, GCNode* unreferencee)
+        : unreferencer(unreferencer), neighborIterator(unreferencer->neighbors().cbegin()), unreferencee(unreferencee) { }
+
+private:
+    GCNode* unreferencer;
+    std::list<GCNode*>::const_iterator neighborIterator;
+    GCNode* unreferencee;
+
+public:
+    bool step() override {
+        if (neighborIterator == unreferencer->neighbors().cend())
+            return true;
+
+        GCNode* node = *neighborIterator;
+        if (node == unreferencee)
+            neighborIterator = unreferencer->neighbors().erase(neighborIterator);
+        neighborIterator++;
+        return false;
+    }
+
+};
+
+} // Spark
