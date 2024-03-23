05f65b22e829eddf6bab10fcff7852e7fca1bbd5
Dongxin Tian <70252050+Dongxin-Tian@users.noreply.github.com>
2024-03-23 14:14:31 -0400

    Implemented 'ReferenceOperation'

--- Spark/GC/ReferenceOperation.hpp
+++ Spark/GC/ReferenceOperation.hpp
@@ -1,3 +1,26 @@
 #pragma once
 
+#include "GCNode.hpp"
+#include "GCOperation.hpp"
 
+namespace Spark {
+
+class ReferenceOperation : public GCOperation {
+
+public:
+    ReferenceOperation(GCNode* referencer, GCNode* referencee) : referencer(referencer), referencee(referencee) { }
+
+private:
+    GCNode* referencer;
+    GCNode* referencee;
+
+public:
+    bool step() override {
+        referencee->referenceCount++;
+        referencer->neighbors().push_back(referencee);
+        return true;
+    }
+
+};
+
+} // Spark
