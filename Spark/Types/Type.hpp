90129e504dbd1d1902b2b92b986dc82c382268bc
Dongxin Tian <70252050+Dongxin-Tian@users.noreply.github.com>
2024-03-23 11:11:30 -0400

    Update Type.hpp

--- Spark/Types/Type.hpp
+++ Spark/Types/Type.hpp
@@ -1,13 +1,21 @@
 #pragma once
 
+#include <cstdint>
+
 namespace Spark {
 
-enum class Type : int {
+enum class Type : uint8_t {
     Nil     = 0x00,
     Integer = 0x01,
     Float   = 0x02,
     Boolean = 0x03,
-    Type    = 0x04
+    Type    = 0x04,
+    String  = 0x05,
+    Array   = 0x06,
+    Set     = 0x07,
+    Map     = 0x08,
+    Object  = 0x09,
+    Thread  = 0x0A
 };
 
 } // Spark
