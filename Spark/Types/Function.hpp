726acfef002fb84ee99f78642a6e0d86a000b85a
Dongxin Tian <70252050+Dongxin-Tian@users.noreply.github.com>
2024-03-29 21:05:19 -0400

    Created 'Function' for Spark function type

--- Spark/Types/Function.hpp
+++ Spark/Types/Function.hpp
@@ -1,9 +1,42 @@
 #pragma once
 
+#include <cstring>
+#include <string>
+
+#include "../Opcode.hpp"
+#include "Value.hpp"
+
 namespace Spark {
 
-class Thread;
+class Function {
 
-using Function = int (*)(Thread*);
+    /* ===== Data ===== */
+
+private:
+    void* _programCounter;
+
+public:
+    [[nodiscard]]
+    constexpr const void* programCounter() const {
+        return _programCounter;
+    }
+
+
+
+    /* ===== Constructor & Destructor ===== */
+
+public:
+    Function(void* programCounter, size_t size) {
+        _programCounter = std::malloc(size);
+        if (!_programCounter)
+            throw std::bad_alloc();
+        std::memcpy(_programCounter, programCounter, size);
+    }
+
+    ~Function() {
+        std::free(_programCounter);
+    }
+
+};
 
 } // Spark
