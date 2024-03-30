06dc850e04074b77b3c976a1de14413ec413aeb1
Dongxin Tian <70252050+Dongxin-Tian@users.noreply.github.com>
2024-03-29 20:24:10 -0400

    '==' operator

--- Spark/Types/Value.cpp
+++ Spark/Types/Value.cpp
@@ -4,22 +4,30 @@
 #include <sstream>
 
 #include "../GC/GC.hpp"
+#include "Closure.hpp"
 
 namespace Spark {
 
     /* ===== Factory Methods ===== */
 
-    Value Value::makeString(Spark::GC& gc, const std::string& value) {
+    Value Value::makeString(GC& gc, const std::string& value) {
         Value self;
         self.type = Type::String;
         self.nodePtr = gc.allocate<std::string>(value);
         return self;
     }
 
-    Value Value::makeArray(Spark::GC& gc, const std::vector<Value>& value) {
+    Value Value::makeArray(GC& gc, const std::vector<Value>& value) {
         Value self;
         self.type = Type::Array;
         self.nodePtr = gc.allocate<std::vector<Value>>(value);
+        return self;
+    }
+
+    Value Value::makeClosure(GC& gc, const Closure& value) {
+        Value self;
+        self.type = Type::Closure;
+        self.nodePtr = gc.allocate<Closure>(value);
         return self;
     }
 
@@ -272,6 +280,71 @@ namespace Spark {
         std::ostringstream ss;
         ss << "Operator '%' is not supported between " << type << " and " << other.type << ".";
         throw std::runtime_error(ss.str());
+    }
+
+    bool Value::operator==(const Value& other) const {
+        // Check equality for numerical types
+        switch (type) {
+            case Type::Integer: {
+                switch (other.type) {
+                    case Type::Integer:
+                        return intValue == other.intValue;
+
+                    case Type::Float:
+                        return static_cast<Float>(intValue) == other.floatValue;
+
+                    default:
+                        break;
+                }
+            }
+                break;
+
+            case Type::Float: {
+                switch (other.type) {
+                    case Type::Integer:
+                        return floatValue == static_cast<Float>(other.intValue);
+
+                    case Type::Float:
+                        return floatValue == other.floatValue;
+
+                    default:
+                        break;
+                }
+            }
+                break;
+
+            default:
+                break;
+        }
+
+        // Check equality for other types
+        if (type != other.type)
+            return false;
+        switch (type) {
+            case Type::Nil:
+                return true;
+
+            case Type::Boolean:
+                return boolValue == other.boolValue;
+
+            case Type::Type:
+                return typeValue == other.typeValue;
+
+            case Type::String:
+                return nodePtr->getData<std::string>() == other.nodePtr->getData<std::string>();
+
+            case Type::Array:
+                return nodePtr->getData<std::vector<Value>>() == other.nodePtr->getData<std::vector<Value>>();
+
+            case Type::Set:
+                return nodePtr->getData<std::unordered_set<Value>>() == other.nodePtr->getData<std::unordered_set<Value>>();
+
+            case Type::Map:
+                return nodePtr->getData<std::unordered_map<Value, Value>>() == other.nodePtr->getData<std::unordered_map<Value, Value>>();
+
+            default:
+                return nodePtr == other.nodePtr;
+        }
     }
 
 } // Spark
