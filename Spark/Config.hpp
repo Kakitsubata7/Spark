e337e34c07b9d365b474d15fbcbc313df269dbf8
Dongxin Tian <70252050+Dongxin-Tian@users.noreply.github.com>
2024-04-23 17:40:42 -0400

    Remove old constants

--- Spark/Config.hpp
+++ Spark/Config.hpp
@@ -2,15 +2,7 @@
 
 #include <cstddef>
 
-#include "Types/Value.hpp"
-
 namespace Spark {
-
-    constexpr size_t DEFAULT_OPERATION_STACK_CAPACITY = 256 * sizeof(Value);
-    constexpr size_t DEFAULT_MAX_OPERATION_STACK_CAPACITY = 131072 * sizeof(Value);
-
-    constexpr size_t DEFAULT_VARIABLE_STACK_CAPACITY = 256 * sizeof(Value);
-    constexpr size_t DEFAULT_MAX_VARIABLE_STACK_CAPACITY = 131072 * sizeof(Value);
 
     constexpr size_t DEFAULT_STACK_CAPACITY = 256;
     constexpr size_t DEFAULT_STACK_MAX_CAPACITY = 131072;
