5dfcbdb7348f186752ef87c8966e1d4d1f8f2e2a
Dongxin Tian <70252050+Dongxin-Tian@users.noreply.github.com>
2024-04-23 17:58:45 -0400

    Created 'InterpretedVM'

---
+++ Spark/InterpretedVM.cpp
@@ -0,0 +1,31 @@
+#include "InterpretedVM.hpp"
+
+#include <stdexcept>
+
+#include "GC/ConcurrentGC.hpp"
+#include "GC/SingleThreadedGC.hpp"
+
+namespace Spark {
+
+    /* ===== Constructor ===== */
+
+    InterpretedVM::InterpretedVM(GCType gcType, size_t mainThreadStackCapacity, size_t mainThreadStackMaxCapacity) {
+        // Create GC
+        switch (gcType) {
+            case GCType::SingleThreaded:
+                gc = std::make_unique<SingleThreadedGC>();
+                break;
+
+            case GCType::Concurrent:
+                gc = std::make_unique<ConcurrentGC>();
+                break;
+
+            default:
+                throw std::runtime_error("GC type is not implemented.");
+        }
+
+        // Create main thread
+        mainThread = std::make_unique<NewThread>(*gc, mainThreadStackCapacity, mainThreadStackMaxCapacity);
+    }
+
+} // Spark
---
+++ Spark/InterpretedVM.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <memory>
+#include <unordered_set>
+
+#include "Config.hpp"
+#include "GC/GCType.hpp"
+#include "NewThread.hpp"
+
+namespace Spark {
+
+class GCBase;
+
+class InterpretedVM {
+
+    /* ===== Thread ===== */
+
+private:
+    std::unique_ptr<NewThread> mainThread;
+    std::unordered_set<std::unique_ptr<NewThread>> threadSet;
+
+
+
+    /* ===== GC ===== */
+
+private:
+    std::unique_ptr<GCBase> gc;
+
+
+
+    /* ===== Constructor ===== */
+
+public:
+    explicit InterpretedVM(GCType gcType,
+                           size_t mainThreadCapacity = DEFAULT_STACK_CAPACITY,
+                           size_t mainThreadMaxCapacity = DEFAULT_STACK_MAX_CAPACITY);
+
+};
+
+} // Spark
