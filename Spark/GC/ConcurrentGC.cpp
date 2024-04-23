a358fe71c4ca2cfd266f89cd4b181de1f572f709
Dongxin Tian <70252050+Dongxin-Tian@users.noreply.github.com>
2024-04-23 17:59:03 -0400

    Created 'ConcurrentGC'

---
+++ Spark/GC/ConcurrentGC.cpp
@@ -0,0 +1,15 @@
+#include "ConcurrentGC.hpp"
+
+namespace Spark {
+
+    /* ===== Operations ===== */
+
+    void ConcurrentGC::addEntryNode(GCNode* node) {
+
+    }
+
+    void ConcurrentGC::removeEntryNode(GCNode* node) {
+
+    }
+
+} // Spark
