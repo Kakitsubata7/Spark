989fb09ff83d6723a582e0a09f8b5121c3fc8d70
Dongxin Tian <70252050+Dongxin-Tian@users.noreply.github.com>
2024-04-27 03:00:31 -0400

    Added header

--- Spark/Compilation/Lex/Token.cpp
+++ Spark/Compilation/Lex/Token.cpp
@@ -2,6 +2,8 @@
 
 namespace Spark {
 
+    /* ===== Operator ===== */
+
     std::ostream& operator<<(std::ostream& os, const Token& token) {
         os << "[" << token.type << ": \"" << token.content << "\"]";
         return os;
