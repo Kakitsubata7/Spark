550dffb14f8baca4a82d625d48f8b6561fd8a482
Dongxin Tian <70252050+Dongxin-Tian@users.noreply.github.com>
2024-05-03 07:51:14 -0400

    Binary expression node

---
+++ Spark/Compilation/Parse/AST/Expressions/BinaryExpression.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <memory>
+
+#include "ExpressionNode.hpp"
+
+namespace Spark {
+
+class BinaryExpression : public ExpressionNode {
+
+    /* ===== Expressions ===== */
+
+protected:
+    std::unique_ptr<ExpressionNode> left;
+    std::unique_ptr<ExpressionNode> right;
+
+
+
+    /* ===== Constructor ===== */
+
+public:
+    BinaryExpression(std::unique_ptr<ExpressionNode>& left, std::unique_ptr<ExpressionNode>& right)
+        : left(std::move(left)), right(std::move(right)) { }
+
+};
+
+} // Spark
