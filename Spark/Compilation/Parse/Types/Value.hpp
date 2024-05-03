aadb50ecdb1782629ee173e0e42baf59a1cdc406
Dongxin Tian <70252050+Dongxin-Tian@users.noreply.github.com>
2024-05-03 07:50:24 -0400

    Added factory methods and operators

--- Spark/Compilation/Parse/Types/Value.hpp
+++ Spark/Compilation/Parse/Types/Value.hpp
@@ -2,6 +2,7 @@
 
 #include <string>
 
+#include "Bool.hpp"
 #include "Int.hpp"
 #include "Float.hpp"
 #include "Type.hpp"
@@ -18,31 +19,50 @@ public:
     union {
         Int intValue{};
         Float floatValue;
+        Bool boolValue;
         std::string strValue;
     };
 
 
 
-    /* ===== Constructors & Destructor ===== */
+    /* ===== Constructors, Factory Methods & Destructor ===== */
 
 public:
     Value() = default;
+
     Value(const Value& other);
     Value(Value&& other) noexcept;
 
     ~Value();
+
+    [[nodiscard]]
+    inline static Value makeNil();
+
+    [[nodiscard]]
+    inline static Value makeInt(Int i);
+
+    [[nodiscard]]
+    inline static Value makeFloat(Float f);
+
+    [[nodiscard]]
+    inline static Value makeBool(Bool b);
+
+    [[nodiscard]]
+    inline static Value makeString(const std::string& str);
 
 
 
     /* ===== Operators ===== */
 
 public:
-    Value operator+(const Value& other);
-    Value operator-(const Value& other);
-    Value operator*(const Value& other);
-    Value operator/(const Value& other);
-    Value operator%(const Value& other);
+    Value operator+(const Value& other) const;
+    Value operator-(const Value& other) const;
+    Value operator*(const Value& other) const;
+    Value operator/(const Value& other) const;
+    Value operator%(const Value& other) const;
 
 };
+
+std::ostream& operator<<(std::ostream& os, const Value& value);
 
 } // Spark
