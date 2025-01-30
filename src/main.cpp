e67f468d0ab7071ad781392c000ed4faa19f59d4
Dongxin Tian <70252050+kakitsubata7@users.noreply.github.com>
2025-01-30 01:59:00 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,19 @@
 #include <cstdlib>
 #include <iostream>
 
-#include "lexer.hpp"
+#include "compiler/lexer.hpp"
 
 using namespace spark;
 
-int main() {
-
+int main(int argc, const char* argv[]) {
     Lexer lexer;
+    Result<std::vector<Token>> result = lexer.lex("if condition do statement else statement end");
+
+    if (result.value.has_value()) {
+        for (const Token& token : result.value.value()) {
+            std::cout << "{" << static_cast<int>(token.type) << ": " << token.lexeme << "}" << std::endl;
+        }
+    }
 
     return EXIT_SUCCESS;
 }
