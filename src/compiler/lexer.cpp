edd05138965bbfea95b203bbbc6b7ae933248be3
Dongxin Tian <70252050+kakitsubata7@users.noreply.github.com>
2025-01-30 01:58:50 -0500

    lexer placeholders

---
+++ src/compiler/lexer.cpp
@@ -0,0 +1,147 @@
+#include "lexer.hpp"
+
+#include <algorithm>
+#include <array>
+#include <istream>
+
+namespace spark {
+
+/* ===== Syntax ===== */
+
+namespace {
+
+bool isSyntaxInitialized = false;
+
+std::array<std::string_view, 27> keywords = {
+    "do", "end",
+    "if", "else",
+    "match", "case",
+    "for", "while", "continue", "break",
+    "let", "const",
+    "fn",
+    "not", "and", "or",
+    "undefined", "nil", "true", "false",
+    "import", "export",
+    "class", "struct", "union", "enum", "alias"
+};
+
+std::array<std::string_view, 27> symbols = {
+    ",", ";",
+    ".", ":",
+    "=",
+    "+", "-", "*", "/", "%", "&", "|", "^", "~",
+    "==", "!=", "<", ">", "<=", ">=",
+    "(", ")", "[", "]", "{", "}",
+    "@"
+};
+
+}
+
+void Lexer::initializeSyntax() {
+    if (isSyntaxInitialized) { return; }
+    isSyntaxInitialized = true;
+
+    std::sort(keywords.begin(), keywords.end());
+}
+
+
+
+/* ===== Operations ===== */
+
+namespace {
+
+[[nodiscard]]
+bool isKeyword(const std::string& s) {
+    return std::binary_search(keywords.cbegin(), keywords.cend(), s);
+}
+
+enum class Match {
+    Possible,
+    Equal,
+    Impossible
+};
+
+[[nodiscard]]
+Match matchKeyword(const std::string& s) {
+    const std::string_view* it = std::lower_bound(keywords.cbegin(), keywords.cend(), s);
+    if (it != keywords.cend() && it->compare(0, s.length(), s.c_str()) == 0) {
+        if (s.length() < it->length()) {
+            return Match::Possible;
+        }
+        return Match::Equal;
+    }
+    return Match::Impossible;
+}
+
+[[nodiscard]]
+Token tokenizeKeyword(const std::string& s) {
+    if (s == "true" || s == "false") {
+        return {TokenType::Boolean, s};
+    }
+    if (s == "nil") {
+        return {TokenType::Nil, s};
+    }
+    return {TokenType::Keyword, s};
+}
+
+[[nodiscard]]
+bool isIdentifier(const std::string& s) {
+    // Empty string is not identifier
+    if (s.empty()) {
+        return false;
+    }
+
+    // First character must be letters or underscore
+    if (!std::isalpha(s[0]) && s[0] != '_') {
+        return false;
+    }
+
+    // The rest characters must be letters, digits, or underscore
+    for (size_t i = 1; i < s.size(); i++) {
+        if (!std::isalnum(s[i]) && s[i] != '_') {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+[[nodiscard]]
+Token tokenizeIdentifier(const std::string& s) {
+    if (s == "_") {
+        return {TokenType::Ignore, "_"};
+    }
+    return {TokenType::Identifier, s};
+}
+
+[[nodiscard]]
+bool isDelimiter(char c) {
+    if (std::isspace(c)) {
+        return true;
+    }
+    if () {
+        return true;
+    }
+    return false;
+}
+
+}
+
+Result<std::vector<Token>> Lexer::lex(std::istream& is) {
+    Result<std::vector<Token>> result;
+    std::vector<Token> tokens;
+
+    std::string current;
+
+    // General matching
+    while (true) {
+
+    }
+
+    // Set lexing result
+    result.value = result.hasError() ? std::nullopt : std::make_optional<std::vector<Token>>(tokens);
+
+    return result;
+}
+
+} // spark
---
+++ src/compiler/lexer.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <sstream>
+#include <vector>
+
+#include "result.hpp"
+#include "token.hpp"
+
+namespace spark {
+
+class Lexer {
+
+    /* ===== Syntax ===== */
+
+public:
+    static void initializeSyntax();
+
+
+
+    /* ===== Data ===== */
+
+private:
+    bool ignoreComments;
+
+
+
+    /* ===== Constructor ===== */
+
+public:
+    explicit Lexer(bool ignoreComments = true) : ignoreComments(ignoreComments) {
+        initializeSyntax();
+    }
+
+
+
+    /* ===== Operations ===== */
+
+public:
+    Result<std::vector<Token>> lex(std::istream& is);
+
+    Result<std::vector<Token>> lex(const std::string& s) {
+        std::istringstream iss(s);
+        return lex(iss);
+    }
+
+};
+
+} // spark
