0bad6cdd29984d4b829dfdecf240d8bce379a601
Dongxin Tian <70252050+kakitsubata7@users.noreply.github.com>
2025-02-01 02:13:48 -0500

    Trie::match

---
+++ src/utils/trie.cpp
@@ -0,0 +1,33 @@
+﻿#include "trie.hpp"
+
+#include <algorithm>
+
+namespace spark {
+
+/* ===== Operation ===== */
+
+Trie::Match Trie::match(std::string_view s) const {
+    Node& root = *(this->root);
+    Node& node = root;
+    for (char c : s) {
+        auto it = std::lower_bound(node.children.begin(), node.children.end(), c,
+                                                     [](const std::unique_ptr<Node>& lhs, char rhs) {
+                                                         return lhs->ch < rhs;
+                                                     });
+        // If current character exists, proceed to the next level
+        if (it != node.children.end() && it->get()->ch == c) {
+            node = **it;
+            continue;
+        }
+        // Otherwise, no match
+        return Match::NotFound;
+    }
+
+    if (!node.isEnd) {
+        // Not an entry ending, no match
+        return Match::NotFound;
+    }
+    return node.children.empty() ? Match::Complete : Match::Prefix;
+}
+
+} // spark
---
+++ src/utils/trie.hpp
@@ -0,0 +1,61 @@
+﻿#pragma once
+
+#include <memory>
+#include <string_view>
+#include <vector>
+
+namespace spark {
+
+/**
+ * This class is implemented as a read-only trie that supports matching @c std::string  while knowing if it is the
+ * longest possible match.
+ */
+class Trie {
+
+    /* ===== Types ==== */
+
+private:
+    struct Node {
+        char ch;
+        bool isEnd;
+        std::vector<std::unique_ptr<Node>> children;
+
+        explicit Node(char ch, bool isEnd) : ch(ch), isEnd(isEnd) { }
+    };
+
+public:
+    enum class Match {
+        NotFound,
+        Prefix,
+        Complete
+    };
+
+
+
+    /* ===== Data ===== */
+
+private:
+    std::unique_ptr<Node> root;
+
+
+
+    /* ===== Constructor ===== */
+
+public:
+    template <typename It>
+    Trie(It first, It last) {
+        // TODO: Build trie
+    }
+
+    Trie() : root(std::make_unique<Node>('\0', false)) { }
+
+
+
+    /* ===== Operation ===== */
+
+public:
+    [[nodiscard]] Match match(std::string_view s) const;
+
+};
+
+} // spark
