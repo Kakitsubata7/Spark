d8442c4cb157aae93c84a09b0fe374ddbac7e7a5
Dongxin Tian <70252050+kakitsubata7@users.noreply.github.com>
2025-02-02 00:57:05 -0500

    Trie::match

--- src/utils/trie.cpp
+++ src/utils/trie.cpp
@@ -4,17 +4,35 @@
 
 namespace spark {
 
-/* ===== Operation ===== */
+/* ===== Operations ===== */
+
+void Trie::append(std::string_view s) {
+    Node* node = &root;
+    for (size_t i = 0; i < s.length(); i++) {
+        char c = s[i];
+
+        auto it = std::lower_bound(node->children.begin(), node->children.end(), c,
+            [](const Node& lhs, char rhs) { return lhs.ch < rhs; });
+        // If current character exists, proceed to the next level
+        if (it != node->children.end() && it->ch == c) {
+            node = &(*it);
+            if (i == s.length() - 1) {
+                node->isEnd = true;  // Mark as complete word
+            }
+            continue;
+        }
+        // Otherwise, insert a new node
+        node = &(*(node->children.insert(it, Node(c, i == s.length() - 1))));
+    }
+}
 
 Trie::Match Trie::match(std::string_view s) const {
     const Node* node = &(this->root);
     for (char c : s) {
         auto it = std::lower_bound(node->children.cbegin(), node->children.cend(), c,
-                                                     [](const Node& lhs, char rhs) {
-                                                         return lhs.ch < rhs;
-                                                     });
+            [](const Node& lhs, char rhs) { return lhs.ch < rhs; });
         // If current character exists, proceed to the next level
-        if (it != node->children.end() && it->ch == c) {
+        if (it != node->children.cend() && it->ch == c) {
             node = &(*it);
             continue;
         }
--- src/utils/trie.hpp
+++ src/utils/trie.hpp
@@ -3,6 +3,8 @@
 #include <string_view>
 #include <vector>
 
+#include <iostream>
+
 namespace spark {
 
 /**
@@ -38,19 +40,24 @@ private:
 
 
 
-    /* ===== Constructor ===== */
+    /* ===== Constructors ===== */
 
 public:
     template <typename It>
     Trie(It first, It last) : root('\0', false) {
-        // TODO: Build trie
+        for (It curr = first; curr != last; ++curr) {
+            append(*curr);
+        }
     }
 
     Trie() : root('\0', false) { }
 
 
 
-    /* ===== Operation ===== */
+    /* ===== Operations ===== */
+
+private:
+    void append(std::string_view s);
 
 public:
     [[nodiscard]] Match match(std::string_view s) const;
