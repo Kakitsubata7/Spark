69cfd1f78eeda316a535b6fc001185cdb150eb9a
Dongxin Tian <kakitsubata0110@gmail.com>
2025-08-08 23:24:19 +0800

    RCInt & RCHeader

---
+++ src/runtime/rc.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <cstdint>
+
+#include "type.hpp"
+
+namespace Spark::Runtime {
+
+/**
+ * Integer type used for reference counting.
+ */
+using RCInt = uintptr_t;
+
+/**
+ * Represents the header of a Spark object that uses reference counting.
+ */
+struct RCHeader {
+    RCInt refCount;
+    const Type* type;
+};
+
+} // Spark::Runtime
