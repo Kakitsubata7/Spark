9012e4d7990ff57a3297356560d69ca7446f57c0
Dongxin Tian <kakitsubata0110@gmail.com>
2025-10-15 10:30:13 -0400

    Pseudocode

--- src/frontend/lexer.cpp
+++ src/frontend/lexer.cpp
@@ -6,18 +6,52 @@ using namespace Spark;
 using namespace Spark::FrontEnd;
 
 Result<Token> Lexer::lex() const {
-    std::string lexeme;
-
-    char c;
-    while (_stream.get(c)) {
-        if (std::isspace(c)) {
-            continue;
+    // Skip space characters
+    char current;
+    while (true) {
+        if (!_stream.get(current)) {
+            // TODO: return error
         }
-
-        lexeme += c;
+        if (!std::isspace(current)) {
+            break;
+        }
     }
+
+    // Check for comment syntax
+    char next;
+    if (_stream.get(next)) {
+        _stream.unget().unget();
+        if (current == '/' && (next == '/' || next == '*')) {
+            return lexComment(_stream);
+        }
+    }
+
+    // Check for other token types
+    _stream.unget();
+    if (std::isalpha(current) || current == '_') {
+        return lexWord(_stream);
+    }
+    if (std::isdigit(current)) {
+        return lexNumber(_stream);
+    }
+    if (current == '"' || current == '\'') {
+        return lexString(_stream);
+    }
+    if (std::ispunct(current)) {
+        return lexPunctuator(_stream);
+    }
+
+    // TODO: return error;
 }
 
 Result<std::vector<Token>> Lexer::lexAll() const {
-
+    std::vector<Token> tokens;
+    while (_stream) {
+        Result<Token> r = lex();
+        if (r.hasError()) {
+            // TODO: return error;
+        }
+        tokens.push_back(r.value());
+    }
+    // TODO: return tokens;
 }
