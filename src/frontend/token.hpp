fb198de4dc3f6b51b80c7db4281dfec3f46e4445
Dongxin Tian <70252050+kakitsubata7@users.noreply.github.com>
2025-10-17 21:12:20 -0400

    Added None TokenType; Constructors for Token

--- src/frontend/token.hpp
+++ src/frontend/token.hpp
@@ -1,16 +1,17 @@
 #pragma once
 
-#include <cstddef>
 #include <string>
 
 namespace Spark::FrontEnd {
 
 enum class TokenType {
+    None,
     Keyword,
     Identifier,
     Discard,
     Operator,
-    Delimiter
+    Delimiter,
+    EndOfFile
 };
 
 struct Token {
@@ -18,6 +19,10 @@ struct Token {
     std::string lexeme;
     size_t line;
     size_t column;
+
+    Token(TokenType type, std::string lexeme, size_t line, size_t column)
+        : type(type), lexeme(std::move(lexeme)), line(line), column(column) { }
+    Token() : type(TokenType::None), line(0), column(0) { }
 };
 
 } // Spark::FrontEnd
