91a192fcffa6d8ef68427d4096a4a5dc48da71de
Dongxin Tian <kakitsubata0110@gmail.com>
2025-10-28 15:04:22 -0400

    WIP

---
+++ src/frontend/lexer_utils.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace Spark::Frontend {
+
+
+
+} // Spark::Frontend
--- src/frontend/token.hpp
+++ src/frontend/token.hpp
@@ -4,32 +4,6 @@
 
 namespace Spark::FrontEnd {
 
-enum class TokenType {
-    None,
-    Keyword,
-    Identifier,
-    Discard,
-    Number,
-    String,
-    Operator,
-    Delimiter,
-    Comment,
-    EndOfFile
-};
 
-struct Token {
-    TokenType type;
-    std::string lexeme;
-    size_t line;
-    size_t column;
-
-    Token(TokenType type, std::string lexeme, size_t line, size_t column)
-        : type(type), lexeme(std::move(lexeme)), line(line), column(column) { }
-    Token() : type(TokenType::None), line(0), column(0) { }
-
-    constexpr bool operator==(const Token& rhs) const {
-        return type == rhs.type && lexeme == rhs.lexeme && line == rhs.line && column == rhs.column;
-    }
-};
 
 } // Spark::FrontEnd
