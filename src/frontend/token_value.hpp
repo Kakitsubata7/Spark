901a14a683bdce3d6f54bdd52aaae9ec9deac8cd
Dongxin Tian <70252050+kakitsubata7@users.noreply.github.com>
2025-11-01 12:42:41 -0400

    Created TokenValue type for yylval

---
+++ src/frontend/token_value.hpp
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include <cstddef>
+#include <string>
+
+namespace Spark::FrontEnd {
+
+struct TokenValue {
+    std::string lexeme;
+    size_t line;
+    size_t column;
+
+    TokenValue() : line(0), column(0) { }
+    TokenValue(std::string lexeme, size_t line, size_t column)
+        : lexeme(std::move(lexeme)), line(line), column(column) { }
+};
+
+} // Spark::FrontEnd
