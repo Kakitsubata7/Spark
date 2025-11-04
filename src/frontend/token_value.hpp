ad66949b0022ed51099532ba3fe7b618adbeb38e
Dongxin Tian <kakitsubata0110@gmail.com>
2025-11-04 14:38:26 -0500

    Added comparison to TokenValue

--- src/frontend/token_value.hpp
+++ src/frontend/token_value.hpp
@@ -5,18 +5,26 @@
 namespace Spark::FrontEnd {
 
 /**
- * Token value type used by Flex and Bison.
+ * Represents a token value type used by Flex and Bison (as `yylval`).
  */
 struct TokenValue {
     std::string lexeme;
     size_t line;
     size_t column;
 
-    TokenValue() : line(0), column(0) { }
-    TokenValue(std::string lexeme, size_t line, size_t column)
+    TokenValue() noexcept : line(0), column(0) { }
+    TokenValue(std::string lexeme, size_t line, size_t column) noexcept
         : lexeme(std::move(lexeme)), line(line), column(column) { }
-    TokenValue(std::string_view lexeme, size_t line, size_t column)
+    TokenValue(std::string_view lexeme, size_t line, size_t column) noexcept
         : TokenValue(std::string(lexeme), line, column) { }
+
+    bool operator==(const TokenValue& rhs) noexcept {
+        return lexeme == rhs.lexeme && line == rhs.line && column == rhs.column;
+    }
+
+    bool operator!=(const TokenValue& rhs) noexcept {
+        return !(*this == rhs);
+    }
 };
 
 } // Spark::FrontEnd
