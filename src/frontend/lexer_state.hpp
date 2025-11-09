c9e6296e3ff984fb2047a89752fb04c2e78ddc0b
Dongxin Tian <70252050+kakitsubata7@users.noreply.github.com>
2025-11-09 16:46:55 -0500

    Documented `LexerState` fields

--- src/frontend/lexer_state.hpp
+++ src/frontend/lexer_state.hpp
@@ -11,13 +11,44 @@ namespace Spark::FrontEnd {
  * Represents additional information used by Flex (as `yyextra`).
  */
 struct LexerState {
+    /**
+     * Current line number of the state.
+     */
     size_t line;
+
+    /**
+     * Current column number of the state.
+     */
     size_t column;
+
+    /**
+     * Pointer to the `std::istream` being processed by the state.
+     */
     std::istream* stream;
+
+    /**
+     * The opening/closing delimiter used by the currently processing string literal.
+     */
     char stringDelimiter;
-    size_t tokenLine;
-    size_t tokenColumn;
+
+    /**
+     * String buffer to store a currently processing token.
+     */
     std::string tokenBuffer;
+
+    /**
+     * Starting line number of the currently processing token.
+     */
+    size_t tokenLine;
+
+    /**
+     * Starting column number of the currently processing token.
+     */
+    size_t tokenColumn;
+
+    /**
+     * Lexing errors encountered by the state.
+     */
     std::vector<LexerError> errors;
 };
 
