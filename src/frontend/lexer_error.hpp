26ef562e8025f164a3520f6d954ab5976dd7f336
Dongxin Tian <70252050+kakitsubata7@users.noreply.github.com>
2025-11-09 22:39:15 -0500

    LexerError == and !=

--- src/frontend/lexer_error.hpp
+++ src/frontend/lexer_error.hpp
@@ -14,6 +14,14 @@ struct LexerError {
 
     LexerError(std::string message, size_t line, size_t column)
         : message(std::move(message)), line(line), column(column) { }
+
+    bool operator==(const LexerError& rhs) const noexcept {
+        return message == rhs.message && line == rhs.line && column == rhs.column;
+    }
+
+    bool operator!=(const LexerError& rhs) const noexcept {
+        return *this == rhs;
+    }
 };
 
 } // Spark::FrontEnd
