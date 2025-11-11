8491b0a9d6ff1063c61d6a1c540546b80d27d263
Dongxin Tian <70252050+kakitsubata7@users.noreply.github.com>
2025-11-11 00:17:04 -0500

    BigReal

--- src/utils/bigint.hpp
+++ src/utils/bigint.hpp
@@ -10,11 +10,11 @@ class BigInt {
 private:
     boost::multiprecision::cpp_int _value;
 
-    explicit BigInt(boost::multiprecision::cpp_int cpp_int) noexcept : _value(std::move(cpp_int)) { }
+    explicit BigInt(boost::multiprecision::cpp_int i) noexcept : _value(std::move(i)) { }
 
 public:
     BigInt() noexcept : _value(0) { }
-    explicit BigInt(std::string_view sv) noexcept : _value(sv) { }
+    explicit BigInt(std::string_view sv) : _value(sv) { }
 
     BigInt operator+(const BigInt& rhs) const { return BigInt(_value + rhs._value); }
     BigInt operator-(const BigInt& rhs) const { return BigInt(_value - rhs._value); }
@@ -28,6 +28,14 @@ public:
     BigInt& operator/=(const BigInt& rhs) { _value /= rhs._value; return *this; }
     BigInt& operator%=(const BigInt& rhs) { _value %= rhs._value; return *this; }
 
+    bool operator==(const BigInt& rhs) const noexcept { return _value == rhs._value; }
+    bool operator!=(const BigInt& rhs) const noexcept { return _value != rhs._value; }
+    bool operator<(const BigInt& rhs)  const noexcept { return _value <  rhs._value; }
+    bool operator<=(const BigInt& rhs) const noexcept { return _value <= rhs._value; }
+    bool operator>(const BigInt& rhs)  const noexcept { return _value >  rhs._value; }
+    bool operator>=(const BigInt& rhs) const noexcept { return _value >= rhs._value; }
+
+    [[nodiscard]]
     std::string str() const {
         return _value.str();
     }
---
+++ src/utils/bigreal.hpp
@@ -0,0 +1,42 @@
+﻿#pragma once
+
+#include <string_view>
+
+#include <boost/multiprecision/cpp_int.hpp>
+
+namespace Spark {
+
+class BigReal {
+private:
+    boost::multiprecision::cpp_rational _value;
+
+    explicit BigReal(boost::multiprecision::cpp_rational r) noexcept : _value(std::move(r)) { }
+
+public:
+    BigReal() noexcept : _value(0) { }
+    explicit BigReal(std::string_view sv) : _value(sv) { }
+
+    BigReal operator+(const BigReal& rhs) const { return BigReal(_value + rhs._value); }
+    BigReal operator-(const BigReal& rhs) const { return BigReal(_value - rhs._value); }
+    BigReal operator*(const BigReal& rhs) const { return BigReal(_value * rhs._value); }
+    BigReal operator/(const BigReal& rhs) const { return BigReal(_value / rhs._value); }
+
+    BigReal& operator+=(const BigReal& rhs) { _value += rhs._value; return *this; }
+    BigReal& operator-=(const BigReal& rhs) { _value -= rhs._value; return *this; }
+    BigReal& operator*=(const BigReal& rhs) { _value *= rhs._value; return *this; }
+    BigReal& operator/=(const BigReal& rhs) { _value /= rhs._value; return *this; }
+
+    bool operator==(const BigReal& rhs) const noexcept { return _value == rhs._value; }
+    bool operator!=(const BigReal& rhs) const noexcept { return _value != rhs._value; }
+    bool operator<(const BigReal& rhs)  const noexcept { return _value <  rhs._value; }
+    bool operator<=(const BigReal& rhs) const noexcept { return _value <= rhs._value; }
+    bool operator>(const BigReal& rhs)  const noexcept { return _value >  rhs._value; }
+    bool operator>=(const BigReal& rhs) const noexcept { return _value >= rhs._value; }
+
+    [[nodiscard]]
+    std::string str() const {
+        return _value.str();
+    }
+};
+
+} // Spark
