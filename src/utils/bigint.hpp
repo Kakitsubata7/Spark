bc587671c0220772bfcded0c8402ae7f2c754e2c
Dongxin Tian <70252050+kakitsubata7@users.noreply.github.com>
2025-11-11 00:07:58 -0500

    Spark::BigInt

---
+++ src/utils/bigint.hpp
@@ -0,0 +1,36 @@
+﻿#pragma once
+
+#include <string_view>
+
+#include <boost/multiprecision/cpp_int.hpp>
+
+namespace Spark {
+
+class BigInt {
+private:
+    boost::multiprecision::cpp_int _value;
+
+    explicit BigInt(boost::multiprecision::cpp_int cpp_int) noexcept : _value(std::move(cpp_int)) { }
+
+public:
+    BigInt() noexcept : _value(0) { }
+    explicit BigInt(std::string_view sv) noexcept : _value(sv) { }
+
+    BigInt operator+(const BigInt& rhs) const { return BigInt(_value + rhs._value); }
+    BigInt operator-(const BigInt& rhs) const { return BigInt(_value - rhs._value); }
+    BigInt operator*(const BigInt& rhs) const { return BigInt(_value * rhs._value); }
+    BigInt operator/(const BigInt& rhs) const { return BigInt(_value / rhs._value); }
+    BigInt operator%(const BigInt& rhs) const { return BigInt(_value % rhs._value); }
+
+    BigInt& operator+=(const BigInt& rhs) { _value += rhs._value; return *this; }
+    BigInt& operator-=(const BigInt& rhs) { _value -= rhs._value; return *this; }
+    BigInt& operator*=(const BigInt& rhs) { _value *= rhs._value; return *this; }
+    BigInt& operator/=(const BigInt& rhs) { _value /= rhs._value; return *this; }
+    BigInt& operator%=(const BigInt& rhs) { _value %= rhs._value; return *this; }
+
+    std::string str() const {
+        return _value.str();
+    }
+};
+
+} // Spark
