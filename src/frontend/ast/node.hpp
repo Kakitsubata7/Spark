e9d29c31e623e0125f2846275e604f07ff38120d
Dongxin Tian <70252050+kakitsubata7@users.noreply.github.com>
2025-11-16 01:05:33 -0500

    Create node.hpp

---
+++ src/frontend/ast/node.hpp
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include <memory>
+
+namespace Spark::FrontEnd::AST {
+
+class NodeVisitor;
+
+class NodeBase {
+public:
+    virtual ~NodeBase() = default;
+
+    virtual void accept(NodeVisitor& v) = 0;
+};
+
+using NodePtr = std::unique_ptr<NodeBase>;
+
+} // Spark::FrontEnd::AST
