76f092d51bfec70d2bd2f677363c44cbaa36fbd2
Dongxin Tian <70252050+kakitsubata7@users.noreply.github.com>
2025-11-17 18:30:44 -0500

    Creates src/frontend/ast/var.hpp

---
+++ src/frontend/ast/var.hpp
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include <optional>
+
+#include "identifier.hpp"
+
+namespace Spark::FrontEnd::AST {
+
+struct VarDecl {
+    enum class DeclType {
+        None, Let, Const, Ref, Cref
+    };
+
+    DeclType declType = DeclType::None;
+    bool isImmutable = false;
+    Pattern* pattern = nullptr;
+    std::optional<Path> typePath;
+};
+
+} // Spark::FrontEnd::AST
