cdaffadccc156077ce5e2039b24eb65837461d6e
Dongxin Tian <70252050+kakitsubata7@users.noreply.github.com>
2025-11-17 18:34:21 -0500

    Adds move semantics to VarDecl and deletes copies

--- src/frontend/ast/var.hpp
+++ src/frontend/ast/var.hpp
@@ -15,6 +15,20 @@ struct VarDecl {
     bool isImmutable = false;
     Pattern* pattern = nullptr;
     std::optional<Path> typePath;
+
+    VarDecl(const VarDecl& other) = delete;
+    VarDecl& operator=(const VarDecl& other) = delete;
+
+    VarDecl(VarDecl&& other) noexcept : pattern(other.pattern) {
+        other.pattern = nullptr;
+    }
+    VarDecl& operator=(VarDecl&& other) noexcept {
+        if (this == &other) {
+            pattern = other.pattern;
+            other.pattern = nullptr;
+        }
+        return *this;
+    }
 };
 
 } // Spark::FrontEnd::AST
