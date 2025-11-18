f5e71564b02b2281e4f8c5bd098e9c58efcd158b
Dongxin Tian <70252050+kakitsubata7@users.noreply.github.com>
2025-11-17 19:36:21 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,5 @@
 #include <cstdlib>
-#include <iostream>
-#include <sstream>
-
-#include "frontend/lexer.hpp"
-#include "frontend/lexer_state.hpp"
-#include "frontend/token.hpp"
-
-using namespace Spark::FrontEnd;
 
 int main(int argc, char* argv[]) {
-    std::string code = R"(
-let x = "abc" if 'xyz\n' > 0 do x = x + 1 end
-    )";
-    std::istringstream iss(code);
-    Lexer lexer(iss);
-
-    while (true) {
-        Token token = lexer.lex();
-        if (token.type == TokenType::EndOfFile) {
-            std::cout << "<EOF>" << std::endl;
-            break;
-        }
-        std::cout << token << "\n";
-    }
-
     return EXIT_SUCCESS;
 }
