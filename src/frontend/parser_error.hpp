1f6c6b50ae9c898a975a68c50311326bd5fa9628
Dongxin Tian <70252050+kakitsubata7@users.noreply.github.com>
2025-11-18 00:17:44 -0500

    Create parser_error.hpp

---
+++ src/frontend/parser_error.hpp
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include <string>
+
+namespace Spark::FrontEnd {
+
+struct ParserError {
+    std::string _message;
+    size_t _line = 0;
+    size_t _column = 0;
+
+    ParserError() = default;
+    ParserError(std::string message, size_t line, size_t column) noexcept
+        : _message(std::move(message)), _line(line), _column(column) { }
+};
+
+} // Spark::FrontEnd
