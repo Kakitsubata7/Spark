9dd1f10227751427d1bc29ee13baac4b3bd1857b
Dongxin Tian <70252050+kakitsubata7@users.noreply.github.com>
2025-11-23 23:58:35 -0500

    Create location.hpp

---
+++ src/utils/location.hpp
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#include <cstddef>
+
+namespace Spark {
+
+struct Location {
+    size_t lineno;
+    size_t columnno;
+
+    constexpr Location() : lineno(0), columnno(0) { }
+    constexpr Location(size_t lineno, size_t columnno) : lineno(lineno), columnno(columnno) { }
+};
+
+} // Spark
