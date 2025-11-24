827ed76d14720d4251522256c2b17d1a3ce1fed5
Dongxin Tian <70252050+kakitsubata7@users.noreply.github.com>
2025-11-23 20:03:30 -0500

    Created SourceBuffer

--- src/compiler/source_buffer.hpp
+++ src/compiler/source_buffer.hpp
@@ -1,5 +1,36 @@
 ﻿#pragma once
 
+#include <string>
+#include <string_view>
+#include <vector>
+
 namespace Spark::Compiler {
+
+/**
+ * Represents a buffer to store and lookup lines from a compiling file.
+ */
+class SourceBuffer {
+private:
+    std::vector<std::string> _lines;
+
+public:
+    /**
+     * Adds a new line to the buffer.
+     * @param line Line string to add.
+     */
+    void addLine(std::string line) {
+        _lines.push_back(std::move(line));
+    }
+
+    /**
+     * Gets the line string from the buffer with line number @p lineno.
+     * @param lineno Line number.
+     * @return Line string at @p lineno.
+     */
+    [[nodiscard]]
+    const std::string& getLine(size_t lineno) const {
+        return _lines[lineno - 1];
+    }
+};
 
 } // Spark::Compiler
