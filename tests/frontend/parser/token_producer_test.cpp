f0351ead7e29bed864268e7262acea9d17ffaaa7
Dongxin Tian <70252050+kakitsubata7@users.noreply.github.com>
2025-12-17 11:12:24 -0500

    Update token_producer_test.cpp

--- tests/frontend/parser/token_producer_test.cpp
+++ tests/frontend/parser/token_producer_test.cpp
@@ -1,4 +1,6 @@
-﻿#include <gtest/gtest.h>
+﻿#include <string_view>
+
+#include <gtest/gtest.h>
 
 #include "frontend/lexer.hpp"
 #include "frontend/parser/token_producer.hpp"
@@ -6,10 +8,9 @@
 using namespace Spark;
 using namespace Spark::FrontEnd;
 
-TEST(LexerTokenProducerTest, Test1) {
-    Lexer lexer("let x = 1");
-    LexerTokenProducer producer(lexer);
+std::string_view test1Code = "let x = 1";
 
+void test1(TokenProducer& producer) {
     Token let(TokenType::Let, "let", {1, 1}, {1, 3});
     Token x(TokenType::Identifier, "x", {1, 5}, {1, 5});
     Token assign(TokenType::Assign, "=", {1, 7}, {1, 7});
@@ -27,6 +28,72 @@ TEST(LexerTokenProducerTest, Test1) {
     EXPECT_EQ(producer.next(), eof);
 }
 
-TEST(RewindLexerTokenProducerTest, Test1) {
+TEST(TokenProducerTest, Test1) {
+    {
+        Lexer lexer(test1Code);
+        LexerTokenProducer producer(lexer);
+        test1(producer);
+    }
 
+    {
+        Lexer lexer(test1Code);
+        RewindLexerTokenProducer producer(lexer);
+        test1(producer);
+    }
+}
+
+std::string_view test2Code = "if foo() then x + 1 else x";
+
+void test2(TokenProducer& producer) {
+    Token tok1(TokenType::If, "if", {1, 1}, {1, 2});
+    Token tok2(TokenType::Identifier, "foo", {1, 4}, {1, 6});
+    Token tok3(TokenType::LParen, "(", {1, 7}, {1, 7});
+    Token tok4(TokenType::RParen, ")", {1, 8}, {1, 8});
+    Token tok5(TokenType::Then, "then", {1, 10}, {1, 13});
+    Token tok6(TokenType::Identifier, "x", {1, 15}, {1, 15});
+    Token tok7(TokenType::Add, "+", {1, 17}, {1, 17});
+    Token tok8(TokenType::Integer, "1", {1, 19}, {1, 19});
+    Token tok9(TokenType::Else, "else", {1, 21}, {1, 24});
+    Token tok10(TokenType::Identifier, "x", {1, 26}, {1, 26});
+    Token eof(TokenType::EndOfFile, "", {1, 26}, {1, 26});
+
+    EXPECT_EQ(producer.peek(), tok1);
+    EXPECT_EQ(producer.next(), tok1);
+
+    EXPECT_EQ(producer.next(), tok2);
+    EXPECT_EQ(producer.next(), tok3);
+    EXPECT_EQ(producer.next(), tok4);
+
+    EXPECT_EQ(producer.peek(), tok5);
+    EXPECT_EQ(producer.next(), tok5);
+
+    EXPECT_EQ(producer.next(), tok6);
+    EXPECT_EQ(producer.next(), tok7);
+    EXPECT_EQ(producer.next(), tok8);
+
+    EXPECT_EQ(producer.peek(), tok9);
+    EXPECT_EQ(producer.peek(), tok9);
+    EXPECT_EQ(producer.peek(), tok9);
+    EXPECT_EQ(producer.next(), tok9);
+
+    EXPECT_EQ(producer.next(), tok10);
+
+    EXPECT_EQ(producer.peek(), eof);
+    EXPECT_EQ(producer.next(), eof);
+    EXPECT_EQ(producer.next(), eof);
+    EXPECT_EQ(producer.peek(), eof);
+}
+
+TEST(TokenProducerTest, Test2) {
+    {
+        Lexer lexer(test2Code);
+        LexerTokenProducer producer(lexer);
+        test2(producer);
+    }
+
+    {
+        Lexer lexer(test2Code);
+        RewindLexerTokenProducer producer(lexer);
+        test2(producer);
+    }
 }
