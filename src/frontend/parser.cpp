6901ac38d891c3f58f7b919539363cdda0616ea6
Dongxin Tian <70252050+kakitsubata7@users.noreply.github.com>
2025-12-17 10:03:42 -0500

    Placeholder Parser::parse for now

--- src/frontend/parser.cpp
+++ src/frontend/parser.cpp
@@ -6,12 +6,13 @@
 namespace Spark::FrontEnd {
 
 Result<AST, Error> Parser::parse(std::istream& stream, std::optional<std::string_view> filename) {
-    Lexer lexer(stream, filename);
-    LexerTokenProducer producer(lexer);
-    AST ast;
-    BodyParser parser(producer, ast);
-    Result<Node*, Error> result = parser.parse();
-    return Result<AST, Error>::ok(std::move(ast));
+    // Lexer lexer(stream, filename);
+    // LexerTokenProducer producer(lexer);
+    // AST ast;
+    // BodyParser parser(producer, ast);
+    // Result<Node*, Error> result = parser.parse();
+    // return Result<AST, Error>::ok(std::move(ast));
+    return Result<AST, Error>::ok(AST());
 }
 
 } // Spark::FrontEnd
