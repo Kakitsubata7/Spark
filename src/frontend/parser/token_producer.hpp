3572036a54b8d69adfcaa8a2a9623ab35328c37a
Dongxin Tian <70252050+kakitsubata7@users.noreply.github.com>
2025-12-16 21:05:19 -0500

    TokenProducer

---
+++ src/frontend/parser/token_producer.hpp
@@ -0,0 +1,40 @@
+﻿#pragma once
+
+#include "frontend/lexer.hpp"
+
+namespace Spark::FrontEnd {
+
+class TokenProducer {
+public:
+    virtual ~TokenProducer() = default;
+
+    virtual Token& next() = 0;
+};
+
+class LexerTokenProducer final : public TokenProducer {
+private:
+    Lexer& _lexer;
+    Token _token;
+
+public:
+    explicit LexerTokenProducer(Lexer& lexer) noexcept : _lexer(lexer) { }
+
+    Token& next() override {
+        return _token = std::move(_lexer.lex());
+    }
+};
+
+class BufferedLexerTokenProducer final : public TokenProducer {
+private:
+    Lexer& _lexer;
+    std::vector<Token> _tokens;
+
+public:
+    explicit BufferedLexerTokenProducer(Lexer& lexer) noexcept : _lexer(lexer) { }
+
+    Token& next() override {
+        return _tokens.emplace_back(std::move(_lexer.lex()));
+    }
+};
+
+} // Spark::FrontEnd
