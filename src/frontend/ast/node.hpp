e102ba1ab648d450b4ef64b040f31b98a45d88e7
Dongxin Tian <kakitsubata0110@gmail.com>
2026-01-22 21:39:21 -0500

    Separated Name from node.hpp

--- src/frontend/ast/node.hpp
+++ src/frontend/ast/node.hpp
@@ -7,6 +7,7 @@
 #include <variant>
 #include <vector>
 
+#include "frontend/name.hpp"
 #include "node_visitor.hpp"
 #include "utils/bigint.hpp"
 #include "utils/bigreal.hpp"
@@ -70,70 +71,6 @@ using Literal = std::variant<VoidLiteral,
                              BoolLiteral,
                              StringLiteral,
                              NilLiteral>;
-
-
-
-struct IdentifierName {
-    std::string name;
-
-    explicit IdentifierName(std::string name) noexcept : name(std::move(name)) { }
-
-    bool operator==(const IdentifierName& rhs) const noexcept { return name == rhs.name; }
-    bool operator!=(const IdentifierName& rhs) const noexcept { return !(*this == rhs); }
-};
-
-struct DiscardName {
-    DiscardName() noexcept = default;
-
-    bool operator==(const DiscardName) const noexcept { return true; }
-    bool operator!=(const DiscardName) const noexcept { return false; }
-};
-
-struct ConstructorName {
-    ConstructorName() noexcept = default;
-
-    bool operator==(const ConstructorName) const noexcept { return true; }
-    bool operator!=(const ConstructorName) const noexcept { return false; }
-};
-
-struct DestructorName {
-    DestructorName() noexcept = default;
-
-    bool operator==(const DestructorName) const noexcept { return true; }
-    bool operator!=(const DestructorName) const noexcept { return false; }
-};
-
-struct OperatorName {
-    enum class OpKind {
-        Add, Sub, Mul, Div, Mod, BitNot, BitAnd, BitOr, BitXor, BitShl, BitShr, LogNot,
-            LogAnd, LogOr, Eq, Ne, Lt, Le, Gt, Ge, Range, RangeExcl,
-        Pos, Neg,
-        Call, Subscript,
-        AddAssign, SubAssign, MulAssign, DivAssign, ModAssign, BitAndAssign, BitOrAssign,
-            BitXorAssign, BitShlAssign, BitShrAssign
-    };
-
-    OpKind op;
-
-    explicit OperatorName(OpKind op) noexcept : op(op) { }
-
-    bool operator==(const OperatorName rhs) const noexcept { return op == rhs.op; }
-    bool operator!=(const OperatorName rhs) const noexcept { return !(*this == rhs); }
-};
-
-struct SelfName {
-    SelfName() noexcept = default;
-
-    bool operator==(const SelfName) const noexcept { return true; }
-    bool operator!=(const SelfName) const noexcept { return false; }
-};
-
-using Name = std::variant<DiscardName,
-                          IdentifierName,
-                          ConstructorName,
-                          DestructorName,
-                          OperatorName,
-                          SelfName>;
 
 
 
---
+++ src/frontend/name.hpp
@@ -0,0 +1,70 @@
+#pragma once
+
+#include <string>
+#include <variant>
+
+namespace Spark::FrontEnd {
+
+struct IdentifierName {
+    std::string name;
+
+    explicit IdentifierName(std::string name) noexcept : name(std::move(name)) { }
+
+    bool operator==(const IdentifierName& rhs) const noexcept { return name == rhs.name; }
+    bool operator!=(const IdentifierName& rhs) const noexcept { return !(*this == rhs); }
+};
+
+struct DiscardName {
+    DiscardName() noexcept = default;
+
+    bool operator==(const DiscardName) const noexcept { return true; }
+    bool operator!=(const DiscardName) const noexcept { return false; }
+};
+
+struct ConstructorName {
+    ConstructorName() noexcept = default;
+
+    bool operator==(const ConstructorName) const noexcept { return true; }
+    bool operator!=(const ConstructorName) const noexcept { return false; }
+};
+
+struct DestructorName {
+    DestructorName() noexcept = default;
+
+    bool operator==(const DestructorName) const noexcept { return true; }
+    bool operator!=(const DestructorName) const noexcept { return false; }
+};
+
+struct OperatorName {
+    enum class OpKind {
+        Add, Sub, Mul, Div, Mod, BitNot, BitAnd, BitOr, BitXor, BitShl, BitShr, LogNot,
+            LogAnd, LogOr, Eq, Ne, Lt, Le, Gt, Ge, Range, RangeExcl,
+        Pos, Neg,
+        Call, Subscript,
+        AddAssign, SubAssign, MulAssign, DivAssign, ModAssign, BitAndAssign, BitOrAssign,
+            BitXorAssign, BitShlAssign, BitShrAssign
+    };
+
+    OpKind op;
+
+    explicit OperatorName(OpKind op) noexcept : op(op) { }
+
+    bool operator==(const OperatorName rhs) const noexcept { return op == rhs.op; }
+    bool operator!=(const OperatorName rhs) const noexcept { return !(*this == rhs); }
+};
+
+struct SelfName {
+    SelfName() noexcept = default;
+
+    bool operator==(const SelfName) const noexcept { return true; }
+    bool operator!=(const SelfName) const noexcept { return false; }
+};
+
+using Name = std::variant<DiscardName,
+                          IdentifierName,
+                          ConstructorName,
+                          DestructorName,
+                          OperatorName,
+                          SelfName>;
+
+} // Spark::FrontEnd
