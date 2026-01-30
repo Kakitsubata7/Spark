ccb8bd192497ebaf62e17b65256023501f56df98
Dongxin Tian <70252050+kakitsubata7@users.noreply.github.com>
2026-01-30 00:14:38 -0500

    Updated NodeVisitor method default implementations

---
+++ src/frontend/ast/node_visitor.cpp
@@ -0,0 +1,75 @@
+﻿#include "node_visitor.hpp"
+
+#include "node.hpp"
+
+namespace Spark::FrontEnd {
+
+void NodeVisitor::visit(Node& node) { }
+
+void NodeVisitor::visit(Expr& node) { visit(static_cast<Node&>(node)); }
+void NodeVisitor::visit(Stmt& node) { visit(static_cast<Node&>(node)); }
+void NodeVisitor::visit(Pattern& node) { visit(static_cast<Node&>(node)); }
+
+void NodeVisitor::visit(VarModifier& node) { visit(static_cast<Node&>(node)); }
+
+void NodeVisitor::visit(FnParam& node) { visit(static_cast<Node&>(node)); }
+void NodeVisitor::visit(FnCapture& node) { visit(static_cast<Node&>(node)); }
+void NodeVisitor::visit(FnCaptureClause& node) { visit(static_cast<Node&>(node)); }
+void NodeVisitor::visit(FnReturn& node) { visit(static_cast<Node&>(node)); }
+
+void NodeVisitor::visit(PathSeg& node) { visit(static_cast<Node&>(node)); }
+void NodeVisitor::visit(Path& node) { visit(static_cast<Node&>(node)); }
+
+void NodeVisitor::visit(LambdaExpr& node) { visit(static_cast<Expr&>(node)); }
+void NodeVisitor::visit(IfThenExpr& node) { visit(static_cast<Expr&>(node)); }
+void NodeVisitor::visit(TryElseExpr& node) { visit(static_cast<Expr&>(node)); }
+void NodeVisitor::visit(MatchCase& node) { visit(static_cast<Node&>(node)); }
+void NodeVisitor::visit(MatchExpr& node) { visit(static_cast<Expr&>(node)); }
+void NodeVisitor::visit(CatchClause& node) { visit(static_cast<Node&>(node)); }
+void NodeVisitor::visit(TryCatchExpr& node) { visit(static_cast<Expr&>(node)); }
+void NodeVisitor::visit(ThrowExpr& node) { visit(static_cast<Expr&>(node)); }
+void NodeVisitor::visit(BlockExpr& node) { visit(static_cast<Expr&>(node)); }
+void NodeVisitor::visit(IsExpr& node) { visit(static_cast<Expr&>(node)); }
+void NodeVisitor::visit(AsExpr& node) { visit(static_cast<Expr&>(node)); }
+void NodeVisitor::visit(BinaryExpr& node) { visit(static_cast<Expr&>(node)); }
+void NodeVisitor::visit(PrefixExpr& node) { visit(static_cast<Expr&>(node)); }
+void NodeVisitor::visit(PostfixExpr& node) { visit(static_cast<Expr&>(node)); }
+void NodeVisitor::visit(MemberAccessExpr& node) { visit(static_cast<Expr&>(node)); }
+void NodeVisitor::visit(CallArg& node) { visit(static_cast<Node&>(node)); }
+void NodeVisitor::visit(CallExpr& node) { visit(static_cast<Expr&>(node)); }
+void NodeVisitor::visit(SubscriptExpr& node) { visit(static_cast<Expr&>(node)); }
+void NodeVisitor::visit(LiteralExpr& node) { visit(static_cast<Expr&>(node)); }
+void NodeVisitor::visit(NameExpr& node) { visit(static_cast<Expr&>(node)); }
+void NodeVisitor::visit(GlobalAccessExpr& node) { visit(static_cast<Expr&>(node)); }
+void NodeVisitor::visit(UpvalueExpr& node) { visit(static_cast<Expr&>(node)); }
+void NodeVisitor::visit(TupleExpr& node) { visit(static_cast<Expr&>(node)); }
+void NodeVisitor::visit(CollectionExpr& node) { visit(static_cast<Expr&>(node)); }
+void NodeVisitor::visit(TypeofExpr& node) { visit(static_cast<Expr&>(node)); }
+
+void NodeVisitor::visit(VarDefStmt& node) { visit(static_cast<Stmt&>(node)); }
+void NodeVisitor::visit(FnDefStmt& node) { visit(static_cast<Stmt&>(node)); }
+void NodeVisitor::visit(TypeDefStmt& node) { visit(static_cast<Stmt&>(node)); }
+void NodeVisitor::visit(CaseDefStmt& node) { visit(static_cast<Stmt&>(node)); }
+void NodeVisitor::visit(AssignStmt& node) { visit(static_cast<Stmt&>(node)); }
+void NodeVisitor::visit(IfStmt& node) { visit(static_cast<Stmt&>(node)); }
+void NodeVisitor::visit(WhileStmt& node) { visit(static_cast<Stmt&>(node)); }
+void NodeVisitor::visit(DoWhileStmt& node) { visit(static_cast<Stmt&>(node)); }
+void NodeVisitor::visit(ForStmt& node) { visit(static_cast<Stmt&>(node)); }
+void NodeVisitor::visit(BreakStmt& node) { visit(static_cast<Stmt&>(node)); }
+void NodeVisitor::visit(ContinueStmt& node) { visit(static_cast<Stmt&>(node)); }
+void NodeVisitor::visit(ReturnStmt& node) { visit(static_cast<Stmt&>(node)); }
+void NodeVisitor::visit(ModuleStmt& node) { visit(static_cast<Stmt&>(node)); }
+void NodeVisitor::visit(ExportStmt& node) { visit(static_cast<Stmt&>(node)); }
+void NodeVisitor::visit(ImportItem& node) { visit(static_cast<Node&>(node)); }
+void NodeVisitor::visit(ImportStmt& node) { visit(static_cast<Stmt&>(node)); }
+void NodeVisitor::visit(ImportAllStmt& node) { visit(static_cast<Stmt&>(node)); }
+void NodeVisitor::visit(UndefineStmt& node) { visit(static_cast<Stmt&>(node)); }
+
+void NodeVisitor::visit(LiteralPattern& node) { visit(static_cast<Pattern&>(node)); }
+void NodeVisitor::visit(BindingPattern& node) { visit(static_cast<Pattern&>(node)); }
+void NodeVisitor::visit(TuplePattern& node) { visit(static_cast<Pattern&>(node)); }
+void NodeVisitor::visit(CollectionPattern& node) { visit(static_cast<Pattern&>(node)); }
+void NodeVisitor::visit(RecordPatternField& node) { visit(static_cast<Node&>(node)); }
+void NodeVisitor::visit(RecordPattern& node) { visit(static_cast<Pattern&>(node)); }
+    
+} // Spark::FrontEnd
--- src/frontend/ast/node_visitor.hpp
+++ src/frontend/ast/node_visitor.hpp
@@ -74,73 +74,73 @@ class NodeVisitor {
 public:
     virtual ~NodeVisitor() = default;
 
-    virtual void visit(Node& node) { }
+    virtual void visit(Node& node);
 
-    virtual void visit(Expr& node) { }
-    virtual void visit(Stmt& node) { }
-    virtual void visit(Pattern& node) { }
+    virtual void visit(Expr& node); 
+    virtual void visit(Stmt& node);
+    virtual void visit(Pattern& node);
 
-    virtual void visit(VarModifier& node) { }
+    virtual void visit(VarModifier& node);
 
-    virtual void visit(FnParam& node) { }
-    virtual void visit(FnCapture& node) { }
-    virtual void visit(FnCaptureClause& node) { }
-    virtual void visit(FnReturn& node) { }
+    virtual void visit(FnParam& node);
+    virtual void visit(FnCapture& node);
+    virtual void visit(FnCaptureClause& node);
+    virtual void visit(FnReturn& node);
 
-    virtual void visit(PathSeg& node) { }
-    virtual void visit(Path& node) { }
+    virtual void visit(PathSeg& node);
+    virtual void visit(Path& node);
 
-    virtual void visit(LambdaExpr& node) { }
-    virtual void visit(IfThenExpr& node) { }
-    virtual void visit(TryElseExpr& node) { }
-    virtual void visit(MatchCase& node) { }
-    virtual void visit(MatchExpr& node) { }
-    virtual void visit(CatchClause& node) { }
-    virtual void visit(TryCatchExpr& node) { }
-    virtual void visit(ThrowExpr& node) { }
-    virtual void visit(BlockExpr& node) { }
-    virtual void visit(IsExpr& node) { }
-    virtual void visit(AsExpr& node) { }
-    virtual void visit(BinaryExpr& node) { }
-    virtual void visit(PrefixExpr& node) { }
-    virtual void visit(PostfixExpr& node) { }
-    virtual void visit(MemberAccessExpr& node) { }
-    virtual void visit(CallArg& node) { }
-    virtual void visit(CallExpr& node) { }
-    virtual void visit(SubscriptExpr& node) { }
-    virtual void visit(LiteralExpr& node) { }
-    virtual void visit(NameExpr& node) { }
-    virtual void visit(GlobalAccessExpr& node) { }
-    virtual void visit(UpvalueExpr& node) { }
-    virtual void visit(TupleExpr& node) { }
-    virtual void visit(CollectionExpr& node) { }
-    virtual void visit(TypeofExpr& node) { }
+    virtual void visit(LambdaExpr& node);
+    virtual void visit(IfThenExpr& node);
+    virtual void visit(TryElseExpr& node);
+    virtual void visit(MatchCase& node);
+    virtual void visit(MatchExpr& node);
+    virtual void visit(CatchClause& node);
+    virtual void visit(TryCatchExpr& node);
+    virtual void visit(ThrowExpr& node);
+    virtual void visit(BlockExpr& node);
+    virtual void visit(IsExpr& node);
+    virtual void visit(AsExpr& node);
+    virtual void visit(BinaryExpr& node);
+    virtual void visit(PrefixExpr& node);
+    virtual void visit(PostfixExpr& node);
+    virtual void visit(MemberAccessExpr& node);
+    virtual void visit(CallArg& node);
+    virtual void visit(CallExpr& node);
+    virtual void visit(SubscriptExpr& node);
+    virtual void visit(LiteralExpr& node);
+    virtual void visit(NameExpr& node);
+    virtual void visit(GlobalAccessExpr& node);
+    virtual void visit(UpvalueExpr& node);
+    virtual void visit(TupleExpr& node);
+    virtual void visit(CollectionExpr& node);
+    virtual void visit(TypeofExpr& node);
 
-    virtual void visit(VarDefStmt& node) { }
-    virtual void visit(FnDefStmt& node) { }
-    virtual void visit(TypeDefStmt& node) { }
-    virtual void visit(CaseDefStmt& node) { }
-    virtual void visit(AssignStmt& node) { }
-    virtual void visit(IfStmt& node) { }
-    virtual void visit(WhileStmt& node) { }
-    virtual void visit(DoWhileStmt& node) { }
-    virtual void visit(ForStmt& node) { }
-    virtual void visit(BreakStmt& node) { }
-    virtual void visit(ContinueStmt& node) { }
-    virtual void visit(ReturnStmt& node) { }
-    virtual void visit(ModuleStmt& node) { }
-    virtual void visit(ExportStmt& node) { }
-    virtual void visit(ImportItem& node) { }
-    virtual void visit(ImportStmt& node) { }
-    virtual void visit(ImportAllStmt& node) { }
-    virtual void visit(UndefineStmt& node) { }
+    virtual void visit(VarDefStmt& node);
+    virtual void visit(FnDefStmt& node);
+    virtual void visit(TypeDefStmt& node);
+    virtual void visit(CaseDefStmt& node);
+    virtual void visit(AssignStmt& node);
+    virtual void visit(IfStmt& node);
+    virtual void visit(WhileStmt& node);
+    virtual void visit(DoWhileStmt& node);
+    virtual void visit(ForStmt& node);
+    virtual void visit(BreakStmt& node);
+    virtual void visit(ContinueStmt& node);
+    virtual void visit(ReturnStmt& node);
+    virtual void visit(ModuleStmt& node);
+    virtual void visit(ExportStmt& node);
+    virtual void visit(ImportItem& node);
+    virtual void visit(ImportStmt& node);
+    virtual void visit(ImportAllStmt& node);
+    virtual void visit(UndefineStmt& node);
 
-    virtual void visit(LiteralPattern& node) { }
-    virtual void visit(BindingPattern& node) { }
-    virtual void visit(TuplePattern& node) { }
-    virtual void visit(CollectionPattern& node) { }
-    virtual void visit(RecordPatternField& node) { }
-    virtual void visit(RecordPattern& node) { }
+    virtual void visit(LiteralPattern& node);
+    virtual void visit(BindingPattern& node);
+    virtual void visit(TuplePattern& node);
+    virtual void visit(CollectionPattern& node);
+    virtual void visit(RecordPatternField& node);
+    virtual void visit(RecordPattern& node);
 };
 
 } // Spark::FrontEnd
