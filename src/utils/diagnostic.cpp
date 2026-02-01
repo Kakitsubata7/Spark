f59755803091e9d637f03b02d144975c5104f46e
Dongxin Tian <70252050+kakitsubata7@users.noreply.github.com>
2026-01-31 23:47:06 -0500

    Spark::Diagnostic

---
+++ src/utils/diagnostic.cpp
@@ -0,0 +1,51 @@
+﻿#include "diagnostic.hpp"
+
+#include <string_view>
+
+namespace Spark {
+
+void Diagnostic::render(std::ostream& os, const FrontEnd::SourceBuffer& srcbuf) const {
+    // Location info
+    if (filename.has_value()) {
+        os << filename.value() << ":";
+    }
+    os << start.line << ":" << start.column << ": ";
+
+    // Message
+    switch (severity) {
+        case Severity::Note:
+            os << "note: ";
+            break;
+        case Severity::Warning:
+            os << "warning: ";
+            break;
+        case Severity::Error:
+            os << "error: ";
+            break;
+        default:
+            break;
+    }
+    os << message << '\n';
+
+    // Snippet
+    if (std::string_view snippet; srcbuf.tryGet(start, end, snippet)) {
+        os << snippet << '\n';
+
+        // // TODO: Highlight
+        // std::string_view line = srcbuf.getLine(start.line);
+        // for (size_t i = 1; i < start.column; ++i) {
+        //     if (line[i - 1] == '\t') {
+        //         os << '\t';
+        //     } else {
+        //         os << ' ';
+        //     }
+        // }
+        // os << '^';
+        // for (size_t i = start.column + 1; i <= end.column; ++i) {
+        //     os << '~';
+        // }
+        // os << '\n';
+    }
+}
+
+} // Spark
---
+++ src/utils/diagnostic.hpp
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+#include <optional>
+#include <ostream>
+#include <string>
+
+#include "frontend/source_buffer.hpp"
+#include "location.hpp"
+
+namespace Spark {
+
+class Diagnostic {
+public:
+    enum class Severity {
+        Note, Warning, Error, Fatal
+    };
+
+    std::optional<std::string> filename;
+    Location start;
+    Location end;
+    Severity severity = Severity::Note;
+    std::string message;
+
+    Diagnostic() = default;
+    Diagnostic(std::optional<std::string> filename,
+               Location start,
+               Location end,
+               Severity severity,
+               std::string message) noexcept
+        : filename(std::move(filename)), start(start), end(end), severity(severity), message(std::move(message)) { }
+
+    void render(std::ostream& os, const FrontEnd::SourceBuffer& srcbuf) const;
+};
+
+} // Spark
