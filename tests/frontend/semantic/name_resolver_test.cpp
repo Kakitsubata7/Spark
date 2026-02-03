6c613b0da7f560b5b956fa87988c2dc57d0f831b
Dongxin Tian <70252050+kakitsubata7@users.noreply.github.com>
2026-02-03 05:35:43 -0500

    Create name_resolver_test.cpp

---
+++ tests/frontend/semantic/name_resolver_test.cpp
@@ -0,0 +1,25 @@
+﻿#include <gtest/gtest.h>
+
+#include "frontend/parser.hpp"
+#include "frontend/semantic/name_resolver.hpp"
+#include "utils/error.hpp"
+
+using namespace Spark;
+using namespace Spark::FrontEnd;
+
+static AST parse(std::string_view source) {
+    std::istringstream iss{std::string(source)};
+    auto [ast, errors] = Parser::parse(iss);
+    EXPECT_TRUE(errors.empty()) << "error during parsing source: \n" << source;
+    return std::move(ast);
+}
+
+TEST(NameResolverTest, Test1) {
+    AST ast = parse(R"(
+    let x = 1;
+    print(x);
+)");
+    SymbolTable symTable;
+    NameResolveResult result = NameResolver::resolve(ast, symTable);
+    EXPECT_TRUE(result.diagnostics.empty());
+}
