9f2659858bdbf1c55f3321ac54aff00285f1d5a1
Dongxin Tian <70252050+kakitsubata7@users.noreply.github.com>
2026-02-03 02:25:14 -0500

    Update symbol.hpp

--- src/frontend/semantic/symbol.hpp
+++ src/frontend/semantic/symbol.hpp
@@ -1,8 +1,15 @@
 ﻿#pragma once
 
+#include "frontend/ast/name.hpp"
+#include "utils/location.hpp"
+
 namespace Spark::FrontEnd {
 
 struct Symbol {
+    InternedNameValue name;
+    Location defStart;
+    Location defEnd;
+
     bool isReassignable;
     bool isReference;
 };
