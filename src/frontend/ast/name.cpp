1ca5cc6960ae5719ba51ecbd0cf1cd2eb6653698
Dongxin Tian <70252050+kakitsubata7@users.noreply.github.com>
2026-02-02 22:40:02 -0500

    reworked name with interning

---
+++ src/frontend/ast/name.cpp
@@ -0,0 +1,73 @@
+﻿#include "name.hpp"
+
+namespace Spark::FrontEnd {
+
+std::string_view NameValue::str() const noexcept {
+    return std::visit([](auto const& v) -> std::string_view {
+        using T = std::decay_t<decltype(v)>;
+
+        if constexpr (std::is_same_v<T, Identifier>) {
+            return v.name;
+        } else if constexpr (std::is_same_v<T, Discard>) {
+            return "_";
+        } else if constexpr (std::is_same_v<T, Self>) {
+            return "self";
+        } else if constexpr (std::is_same_v<T, Constructor>) {
+            return "constructor";
+        } else if constexpr (std::is_same_v<T, Destructor>) {
+            return "destructor";
+        } else if constexpr (std::is_same_v<T, Operator>) {
+            switch (v.op) {
+                case OverOpKind::Add:          return "operator+";
+                case OverOpKind::Sub:          return "operator-";
+                case OverOpKind::Mul:          return "operator*";
+                case OverOpKind::Div:          return "operator/";
+                case OverOpKind::Mod:          return "operator%";
+                case OverOpKind::BitNot:       return "operator~";
+                case OverOpKind::BitAnd:       return "operator&";
+                case OverOpKind::BitOr:        return "operator|";
+                case OverOpKind::BitXor:       return "operator^";
+                case OverOpKind::BitShl:       return "operator<<";
+                case OverOpKind::BitShr:       return "operator>>";
+                case OverOpKind::LogNot:       return "operator!";
+                case OverOpKind::LogAnd:       return "operator&&";
+                case OverOpKind::LogOr:        return "operator||";
+                case OverOpKind::Eq:           return "operator==";
+                case OverOpKind::Ne:           return "operator!=";
+                case OverOpKind::Lt:           return "operator<";
+                case OverOpKind::Le:           return "operator<=";
+                case OverOpKind::Gt:           return "operator>";
+                case OverOpKind::Ge:           return "operator>=";
+                case OverOpKind::Range:        return "operator...";
+                case OverOpKind::RangeExcl:    return "operator..<";
+                case OverOpKind::Pos:          return "operator(+)";
+                case OverOpKind::Call:         return "operator()";
+                case OverOpKind::Subscript:    return "operator[]";
+                case OverOpKind::AddAssign:    return "operator+=";
+                case OverOpKind::SubAssign:    return "operator-=";
+                case OverOpKind::MulAssign:    return "operator*=";
+                case OverOpKind::DivAssign:    return "operator/=";
+                case OverOpKind::ModAssign:    return "operator%=";
+                case OverOpKind::BitAndAssign: return "operator&=";
+                case OverOpKind::BitOrAssign:  return "operator|=";
+                case OverOpKind::BitXorAssign: return "operator^=";
+                case OverOpKind::BitShlAssign: return "operator<<=";
+                case OverOpKind::BitShrAssign: return "operator>>=";
+                default:                       return ""; // Unreachable
+            }
+        }
+        return ""; // Unreachable
+    }, _value);
+}
+
+InternedNameValue NameValueInterner::intern(NameValue value) {
+    std::unique_ptr<NameValue> key = std::make_unique<NameValue>(std::move(value));
+
+    if (auto it = _set.find(key); it != _set.end()) {
+        return InternedNameValue{it->get()};
+    }
+    auto [it, _] = _set.emplace(std::move(key));
+    return InternedNameValue{it->get()};
+}
+
+} // Spark::FrontEnd
---
+++ src/frontend/ast/name.hpp
@@ -0,0 +1,128 @@
+﻿#pragma once
+
+#include <memory>
+#include <string>
+#include <unordered_set>
+#include <variant>
+
+namespace Spark::FrontEnd {
+
+enum class OverOpKind {
+    Add, Sub, Mul, Div, Mod, BitNot, BitAnd, BitOr, BitXor, BitShl, BitShr, LogNot,
+        LogAnd, LogOr, Eq, Ne, Lt, Le, Gt, Ge, Range, RangeExcl,
+    Pos, Neg,
+    Call, Subscript,
+    AddAssign, SubAssign, MulAssign, DivAssign, ModAssign, BitAndAssign, BitOrAssign,
+        BitXorAssign, BitShlAssign, BitShrAssign
+};
+
+class NameValue {
+private:
+    struct Identifier {
+        std::string name;
+        friend bool operator==(const Identifier& lhs, const Identifier& rhs) noexcept { return lhs.name == rhs.name; };
+    };
+    struct Discard { friend bool operator==(const Discard&, const Discard&) noexcept { return true; } };
+    struct Self { friend bool operator==(const Self&, const Self&) noexcept { return true; } };
+    struct Constructor { friend bool operator==(const Constructor&, const Constructor&) noexcept { return true; } };
+    struct Destructor { friend bool operator==(const Destructor&, const Destructor&) noexcept { return true; } };
+    struct Operator {
+        OverOpKind op;
+        friend bool operator==(const Operator& lhs, const Operator& rhs) noexcept { return lhs.op == rhs.op; }
+    };
+
+    using ValueT = std::variant<Identifier, Discard, Self, Constructor, Destructor, Operator>;
+
+    ValueT _value;
+
+    explicit NameValue(ValueT value) noexcept : _value(std::move(value)) { }
+
+public:
+    static NameValue identifier(std::string name) noexcept { return NameValue(Identifier{.name = std::move(name)}); }
+    static NameValue discard() noexcept { return NameValue(Discard{}); }
+    static NameValue self() noexcept { return NameValue(Self{}); }
+    static NameValue constructor() noexcept { return NameValue(Constructor{}); }
+    static NameValue destructor() noexcept { return NameValue(Destructor{}); }
+    static NameValue op(OverOpKind op) noexcept { return NameValue(Operator{.op = op}); }
+
+    bool isIdentifier() const noexcept { return std::holds_alternative<Identifier>(_value); }
+    bool isDiscard() const noexcept { return std::holds_alternative<Discard>(_value); }
+    bool isSelf() const noexcept { return std::holds_alternative<Self>(_value); }
+    bool isConstructor() const noexcept { return std::holds_alternative<Constructor>(_value); }
+    bool isDestructor() const noexcept { return std::holds_alternative<Destructor>(_value); }
+    bool isOp() const noexcept { return std::holds_alternative<Operator>(_value); }
+
+    std::string_view str() const noexcept;
+
+    bool operator==(const NameValue& rhs) const noexcept {
+        return _value == rhs._value;
+    }
+
+    bool operator!=(const NameValue& rhs) const noexcept {
+        return !(*this == rhs);
+    }
+
+    friend std::hash<NameValue>;
+};
+
+} // Spark::FrontEnd
+
+template<>
+struct std::hash<Spark::FrontEnd::NameValue> {
+    size_t operator()(const Spark::FrontEnd::NameValue& n) const noexcept {
+        return std::visit([&](auto const& v) -> size_t {
+            using T = std::decay_t<decltype(v)>;
+
+            size_t tag = n._value.index();
+            if constexpr (std::is_same_v<T, Spark::FrontEnd::NameValue::Identifier>) {
+                return tag ^ (std::hash<std::string>{}(v.name) << 1);
+            } else if constexpr (std::is_same_v<T, Spark::FrontEnd::NameValue::Operator>) {
+                return tag ^ (std::hash<Spark::FrontEnd::OverOpKind>{}(v.op) << 1);
+            } else {
+                return tag;
+            }
+        }, n._value);
+    }
+};
+
+namespace Spark::FrontEnd {
+
+class InternedNameValue {
+private:
+    const NameValue* _ptr;
+
+public:
+    explicit InternedNameValue(const NameValue* ptr) noexcept : _ptr(ptr) { }
+
+    const NameValue& value() const noexcept { return *_ptr; }
+
+    friend bool operator==(InternedNameValue lhs, InternedNameValue rhs) noexcept {
+        return lhs._ptr == rhs._ptr;
+    }
+
+    friend bool operator!=(InternedNameValue lhs, InternedNameValue rhs) noexcept {
+        return !(lhs == rhs);
+    }
+};
+
+class NameValueInterner {
+private:
+    struct PtrHash {
+        size_t operator()(const std::unique_ptr<NameValue>& p) const noexcept {
+            return std::hash<NameValue>{}(*p);
+        }
+    };
+
+    struct PtrEq {
+        bool operator()(const std::unique_ptr<NameValue>& lhs, const std::unique_ptr<NameValue>& rhs) const noexcept {
+            return *lhs == *rhs;
+        }
+    };
+
+    std::unordered_set<std::unique_ptr<NameValue>, PtrHash, PtrEq> _set;
+
+public:
+    InternedNameValue intern(NameValue value);
+};
+
+} // Spark::FrontEnd
