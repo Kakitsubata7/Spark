9dad213652b594d5ca9f57ea6bb538f94abaebe0
Dongxin Tian <70252050+kakitsubata7@users.noreply.github.com>
2026-02-03 01:03:25 -0500

    Env

---
+++ src/frontend/semantic/env.hpp
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include <unordered_map>
+
+#include "frontend/ast/name.hpp"
+#include "symbol.hpp"
+
+namespace Spark::FrontEnd {
+
+/**
+ * Represents a lexical binding environment during name resolution.
+ */
+class Env {
+private:
+    std::unordered_map<InternedNameValue, Symbol*> _map;
+
+public:
+    void define(InternedNameValue name, Symbol* symbol) {
+        _map[name] = symbol;
+    }
+
+    Symbol* get(InternedNameValue name) const {
+        return _map.at(name);
+    }
+};
+
+} // Spark::FrontEnd
