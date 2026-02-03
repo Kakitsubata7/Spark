30ee60ddeaf69b657d0e562e5ee6f9d15679029e
Dongxin Tian <70252050+kakitsubata7@users.noreply.github.com>
2026-02-03 01:06:49 -0500

    semantic/utils.hpp

---
+++ src/frontend/semantic/utils.hpp
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include "frontend/ast/node.hpp"
+
+namespace Spark::FrontEnd {
+
+[[nodiscard]]
+inline bool isHoisted(const Node* node) {
+    return node->is<FnDefStmt>() || node->is<TypeDefStmt>() || node->is<ModuleStmt>();
+}
+
+} // Spark::FrontEnd
