dd1f0c5c4d20e3a73f0316e2a64bb171a5b8b09d
Dongxin Tian <70252050+kakitsubata7@users.noreply.github.com>
2026-02-09 11:48:23 -0500

    Global resolver

---
+++ src/frontend/semantic/global_resolver.hpp
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+#include "env.hpp"
+#include "frontend/ast.hpp"
+#include "symbol.hpp"
+#include "utils/diagnostic.hpp"
+
+namespace Spark::FrontEnd {
+
+class GlobalResolveVisitor : public NodeVisitor {
+public:
+    SymbolTable& symTable;
+    NodeSymbolMap& nodeSymMap;
+    Env& globalEnv;
+
+    GlobalResolveVisitor(SymbolTable& symTable, NodeSymbolMap& nodeSymMap, Env& globalEnv) noexcept
+        : symTable(symTable), nodeSymMap(nodeSymMap), globalEnv(globalEnv) { }
+};
+
+struct GlobalResolveResult {
+    Env globalEnv;
+    Diagnostics diagnostics;
+};
+
+class GlobalResolver {
+public:
+    static GlobalResolveResult resolve(const AST& ast, SymbolTable& symTable, NodeSymbolMap& nodeSymMap);
+};
+
+} // Spark::FrontEnd
