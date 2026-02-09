6706f052140699ffbc40990dfe8755d9c00ee1c5
Dongxin Tian <70252050+kakitsubata7@users.noreply.github.com>
2026-02-09 07:39:33 -0500

    SymbolTable::adopt + documented

--- src/frontend/semantic/symbol.hpp
+++ src/frontend/semantic/symbol.hpp
@@ -64,13 +64,29 @@ public:
     SymbolTable(SymbolTable&&) = default;
     SymbolTable& operator=(SymbolTable&&) = default;
 
+    /**
+     * Creates a `Symbol` instance.
+     * @param symbol Symbol to create.
+     * @return Pointer to the created symbol.
+     */
     Symbol* make(Symbol symbol) {
         return _symbols.emplace_back(std::make_unique<Symbol>(std::move(symbol))).get();
+    }
+
+    /**
+     * Moves all symbols from another symbol table into itself.
+     * @param from Symbol table to move from.
+     */
+    void adopt(SymbolTable& from) {
+        for (std::unique_ptr<Symbol>& p : from._symbols) {
+            _symbols.push_back(std::move(p));
+        }
+        from._symbols.clear();
     }
 };
 
 /**
- * Represents a data structure that maps a node to a symbol.
+ * Represents a data structure that maps an AST node to a symbol.
  */
 class NodeSymbolMap {
 private:
@@ -85,17 +101,32 @@ public:
     NodeSymbolMap(NodeSymbolMap&&) = default;
     NodeSymbolMap& operator=(NodeSymbolMap&&) = default;
 
+    /**
+     * Checks whether an AST node has a symbol.
+     * @param node AST node to check.
+     * @return `true` if the AST node has a symbol, false otherwise.
+     */
     [[nodiscard]]
     bool hasSymbol(const Node* node) const {
         return _map.find(node) != _map.end();
     }
 
+    /**
+     * Retrieves the symbol of the AST node.
+     * @param node AST node to retrieve symbol from.
+     * @return Symbol or `nullptr` if the AST node doesn't have a symbol.
+     */
     [[nodiscard]]
     Symbol* symbolOf(const Node* node) const {
         auto it = _map.find(node);
         return it == _map.end() ? nullptr : it->second;
     }
 
+    /**
+     * Sets the symbol of an AST node.
+     * @param node AST node to set symbol.
+     * @param symbol Symbol to set.
+     */
     void set(const Node* node, Symbol* symbol) {
         _map[node] = symbol;
     }
