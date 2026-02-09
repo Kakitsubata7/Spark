279efd709122b8548abc7413ae4ccb352c51ee42
Dongxin Tian <70252050+kakitsubata7@users.noreply.github.com>
2026-02-09 10:55:32 -0500

    Created binder helper class

---
+++ src/frontend/semantic/binder.cpp
@@ -0,0 +1,86 @@
+﻿#include "binder.hpp"
+
+namespace Spark::FrontEnd {
+
+void BindingVisitor::visit(Name* name) {
+    // Check if name is already declared in the same env
+    if (const Symbol* s = _env.find(name->value); s != nullptr) {
+        if (_isRedeclarable) {
+            _nodeSymMap.set(name, s);
+        } else {
+            report(Diagnostic::redeclareError(name->start, name->end, s));
+        }
+        return;
+    }
+
+    // Define symbol from name
+    const Symbol* s = _symTable.make(Symbol{
+        .node = name,
+        .isReassignable = _isReassignable,
+        .isReference = _isReference
+    });
+    _env.declare(s, _isVisible);
+    _nodeSymMap.set(name, s);
+}
+
+void BindingVisitor::visit(BindingPattern* pattern) {
+    pattern->name->accept(*this);
+}
+
+void BindingVisitor::visit(TuplePattern* pattern) {
+    for (Pattern* cp : pattern->patterns) {
+        cp->accept(*this);
+    }
+}
+
+void BindingVisitor::visit(CollectionPattern* pattern) {
+    for (Pattern* cp : pattern->prefix) {
+        cp->accept(*this);
+    }
+    for (Pattern* cp : pattern->suffix) {
+        cp->accept(*this);
+    }
+}
+
+void BindingVisitor::visit(RecordPattern* pattern) {
+    for (RecordPatternField* field : pattern->fields) {
+        field->pattern->accept(*this);
+    }
+}
+
+
+void Binder::BinderVisitor::visit(VarDefStmt* vardef) {
+    VarModifier* mod = vardef->mod;
+    BindingVisitor v{_env, _symTable, _nodeSymMap, isReassignable(mod), isReference(mod), false, _isVisible, _diagnostics};
+    vardef->pattern->accept(v);
+}
+
+void Binder::BinderVisitor::visit(FnDefStmt* fndef) {
+    BindingVisitor v{_env, _symTable, _nodeSymMap, false, false, true, _isVisible, _diagnostics};
+    fndef->name->accept(v);
+}
+
+void Binder::BinderVisitor::visit(TypeDefStmt* tdef) {
+    BindingVisitor v{_env, _symTable, _nodeSymMap, false, false, false, _isVisible, _diagnostics};
+    tdef->name->accept(v);
+}
+
+void Binder::BinderVisitor::visit(ModuleStmt* moddef) {
+    BindingVisitor v{_env, _symTable, _nodeSymMap, false, false, true, _isVisible, _diagnostics};
+    moddef->path->segs[0]->name->accept(v);
+}
+
+void Binder::bind(Node* node,
+                  Env& env,
+                  SymbolTable& symTable,
+                  NodeSymbolMap& nodeSymMap,
+                  bool isVisible,
+                  Diagnostics& diagnostics) {
+    if (node == nullptr) {
+        return;
+    }
+    BinderVisitor v{env, symTable, nodeSymMap, isVisible, diagnostics};
+    node->accept(v);
+}
+
+} // Spark::FrontEnd
---
+++ src/frontend/semantic/binder.hpp
@@ -0,0 +1,108 @@
+﻿#pragma once
+
+#include "env.hpp"
+#include "symbol.hpp"
+#include "utils/diagnostic.hpp"
+
+namespace Spark::FrontEnd {
+
+/**
+ * Represents a visitor that traverses a node and binds name(s) to symbol(s) to an environment.
+ */
+class BindingVisitor : public NodeVisitor {
+private:
+    Env& _env;
+    SymbolTable& _symTable;
+    NodeSymbolMap& _nodeSymMap;
+
+    bool _isReassignable;
+    bool _isReference;
+    bool _isRedeclarable;
+    bool _isVisible;
+
+    Diagnostics& _diagnostics;
+
+public:
+    BindingVisitor(Env& env,
+                   SymbolTable& symTable,
+                   NodeSymbolMap& nodeSymMap,
+                   bool isReassignable,
+                   bool isReference,
+                   bool isRedeclarable,
+                   bool isVisible,
+                   Diagnostics& diagnostics) noexcept
+        : _env(env), _symTable(symTable), _nodeSymMap(nodeSymMap), _isReassignable(isReassignable),
+          _isReference(isReference), _isRedeclarable(isRedeclarable), _isVisible(isVisible),
+          _diagnostics(diagnostics) { }
+
+    void visit(Name* name) override;
+    void visit(BindingPattern* pattern) override;
+    void visit(TuplePattern* pattern) override;
+    void visit(CollectionPattern* pattern) override;
+    void visit(RecordPattern* pattern) override;
+
+private:
+    /**
+     * Adds a diagnostic to the result.
+     * @param diagnostic Diagnostic to add.
+     */
+    void report(Diagnostic diagnostic) noexcept {
+        _diagnostics.add(std::move(diagnostic));
+    }
+};
+
+/**
+ * Represents a wrapper to `BindingVisitor` that binds a definition node's names to the given environment.
+ */
+class Binder {
+private:
+    class BinderVisitor : public NodeVisitor {
+    private:
+        Env& _env;
+        SymbolTable& _symTable;
+        NodeSymbolMap& _nodeSymMap;
+        bool _isVisible;
+        Diagnostics& _diagnostics;
+
+    public:
+        BinderVisitor(Env& env, SymbolTable& symTable, NodeSymbolMap& nodeSymMap, bool isVisible,
+                      Diagnostics& diagnostics) noexcept
+            : _env(env), _symTable(symTable), _nodeSymMap(nodeSymMap), _isVisible(isVisible),
+              _diagnostics(diagnostics) { }
+
+        void visit(VarDefStmt* vardef) override;
+        void visit(FnDefStmt* fndef) override;
+        void visit(TypeDefStmt* tdef) override;
+        void visit(ModuleStmt* moddef) override;
+
+    private:
+        static bool isReassignable(const VarModifier* mod) noexcept {
+            VarModifier::VarKind kind = mod->kind;
+            return kind == VarModifier::VarKind::Let || kind == VarModifier::VarKind::Ref;
+        }
+
+        static bool isReference(const VarModifier* mod) noexcept {
+            VarModifier::VarKind kind = mod->kind;
+            return kind == VarModifier::VarKind::Ref || kind == VarModifier::VarKind::Cref;
+        }
+    };
+
+public:
+    /**
+     * Binds names of a definition node to the current environment, creating symbols.
+     * @param node Definition node.
+     * @param env Environment to bind names to.
+     * @param symTable Symbol table.
+     * @param nodeSymMap AST node symbol map.
+     * @param isVisible Whether the bound symbols are visible in the environment or not.
+     * @param diagnostics Diagnostics go in here.
+     */
+    static void bind(Node* node,
+                     Env& env,
+                     SymbolTable& symTable,
+                     NodeSymbolMap& nodeSymMap,
+                     bool isVisible,
+                     Diagnostics& diagnostics);
+};
+
+} // Spark::FrontEnd
