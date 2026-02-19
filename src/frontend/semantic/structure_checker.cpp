cf28df9af0743982dd12b8db59a9bdad196b5fb8
Dongxin Tian <70252050+kakitsubata7@users.noreply.github.com>
2026-02-19 00:33:37 -0500

    structural checker

---
+++ src/frontend/semantic/structure_checker.cpp
@@ -0,0 +1,172 @@
+﻿#include "structure_checker.hpp"
+
+namespace Spark::FrontEnd {
+
+Diagnostics StructureChecker::check(const AST& ast) {
+    if (ast.root == nullptr) {
+        return {};
+    }
+
+    Diagnostics diagnostics{};
+    StructureChecker checker{diagnostics};
+    checker.replaceCtx(Context{
+        .inLoop = false,
+        .inFunc = false,
+        .inType = false,
+        .inModule = true
+    });
+    ast.root->accept(checker);
+    return std::move(diagnostics);
+}
+
+void StructureChecker::visit(Expr* expr) {
+    // Fall back for non-declarative expressions
+    if (inType()) {
+        error(expr->start, expr->end, "non-declarative expression is not allowed inside a type declaration body");
+    }
+}
+
+void StructureChecker::visit(Stmt* stmt) {
+    // Fall back for non-declarative statements
+    if (inType()) {
+        error(stmt->start, stmt->end, "non-declarative statement is not allowed inside a type declaration body");
+    }
+}
+
+void StructureChecker::visit(BlockExpr* block) {
+    // Represents a standalone block expression, does not represent a statement's body
+    Context old = replaceCtx(Context{
+        .inLoop = inLoop(),
+        .inFunc = inFunc(),
+        .inType = false,
+        .inModule = false
+    });
+
+    for (Node* node : block->nodes) {
+        node->accept(*this);
+    }
+
+    replaceCtx(old);
+}
+
+void StructureChecker::visit(VarDefStmt* vardef) { }
+
+void StructureChecker::visit(WhileStmt* w) {
+    visitLoop(w->body);
+}
+
+void StructureChecker::visit(ForStmt* f) {
+    visitLoop(f->body);
+}
+
+void StructureChecker::visit(FnDefStmt* fndef) {
+    visitFn(fndef->body);
+}
+
+void StructureChecker::visit(LambdaExpr* lambda) {
+    visitFn(lambda->body);
+}
+
+void StructureChecker::visit(TypeDefStmt* tdef) {
+    Context old = replaceCtx(Context{
+        .inLoop = false,
+        .inFunc = false,
+        .inType = true,
+        .inModule = false
+    });
+
+    for (Node* node : tdef->body->nodes) {
+        node->accept(*this);
+    }
+
+    replaceCtx(old);
+}
+
+void StructureChecker::visit(ModuleStmt* moddef) {
+    if (inLoop() || inFunc() || inType()) {
+        error(moddef->start, moddef->end, "module declaration is only allowed inside a module");
+    }
+
+    Context old = replaceCtx(Context{
+        .inLoop = false,
+        .inFunc = false,
+        .inType = false,
+        .inModule = true
+    });
+
+    for (Node* node : moddef->body->nodes) {
+        node->accept(*this);
+    }
+
+    replaceCtx(old);
+}
+
+void StructureChecker::visit(BreakStmt* b) {
+    if (!inLoop()) {
+        error(b->start, b->end, "`break` is only allowed inside a loop");
+    }
+}
+
+void StructureChecker::visit(ContinueStmt* c) {
+    if (!inLoop()) {
+        error(c->start, c->end, "`continue` is only allowed inside a loop");
+    }
+}
+
+void StructureChecker::visit(ReturnStmt* r) {
+    if (!inFunc()) {
+        error(r->start, r->end, "`return` is only allowed inside a function");
+    }
+}
+
+void StructureChecker::visit(ExportStmt* e) {
+    if (!inModule()) {
+        error(e->start, e->end, "`export` is only allowed inside a module");
+    }
+
+    if (!e->stmt->is<VarDefStmt>() && !e->stmt->is<FnDefStmt>() && !e->stmt->is<TypeDefStmt>() && !e->stmt->is<ModuleStmt>()) {
+        error(e->start, e->end, "cannot export non-declaration statement");
+    }
+}
+
+void StructureChecker::visit(UndefineStmt* undef) {
+    if (!inModule()) {
+        error(undef->start, undef->end, "`undefine` is only allowed inside a module");
+    }
+}
+
+void StructureChecker::visitLoop(BlockExpr* body) {
+    Context old = replaceCtx(Context{
+        .inLoop = true,
+        .inFunc = inFunc(),
+        .inType = false,
+        .inModule = false
+    });
+
+    for (Node* node : body->nodes) {
+        node->accept(*this);
+    }
+
+    replaceCtx(old);
+}
+
+void StructureChecker::visitFn(Expr* body) {
+    Context old = replaceCtx(Context{
+        .inLoop = false,
+        .inFunc = true,
+        .inType = false,
+        .inModule = false
+    });
+
+    if (BlockExpr* block = body->as<BlockExpr>()) {
+        for (Node* node : block->nodes) {
+            node->accept(*this);
+        }
+    } else {
+        body->accept(*this);
+    }
+
+    replaceCtx(old);
+}
+
+} // Spark::FrontEnd
---
+++ src/frontend/semantic/structure_checker.hpp
@@ -0,0 +1,71 @@
+﻿#pragma once
+
+#include "frontend/ast.hpp"
+#include "utils/diagnostic.hpp"
+
+namespace Spark::FrontEnd {
+
+class StructureChecker : public NodeVisitor {
+private:
+    struct Context {
+        bool inLoop;
+        bool inFunc;
+        bool inType;
+        bool inModule;
+    };
+
+    Context _ctx{};
+
+    Diagnostics& _diagnostics;
+
+    explicit StructureChecker(Diagnostics& diagnostics) noexcept : _diagnostics(diagnostics) { }
+
+public:
+    static Diagnostics check(const AST& ast);
+
+    void visit(Expr* expr) override;
+    void visit(Stmt* stmt) override;
+    void visit(BlockExpr* block) override;
+    void visit(VarDefStmt* vardef) override;
+
+    void visit(WhileStmt* w) override;
+    void visit(ForStmt* f) override;
+    void visit(FnDefStmt* fndef) override;
+    void visit(LambdaExpr* lambda) override;
+    void visit(TypeDefStmt* tdef) override;
+    void visit(ModuleStmt* moddef) override;
+
+    void visit(BreakStmt* b) override;
+    void visit(ContinueStmt* c) override;
+    void visit(ReturnStmt* r) override;
+    void visit(ExportStmt* e) override;
+    void visit(UndefineStmt* undef) override;
+
+private:
+    Context replaceCtx(Context ctx) {
+        Context old = _ctx;
+        _ctx = ctx;
+        return old;
+    }
+
+    [[nodiscard]]
+    constexpr bool inLoop() const noexcept { return _ctx.inLoop; }
+
+    [[nodiscard]]
+    constexpr bool inFunc() const noexcept { return _ctx.inFunc; }
+
+    [[nodiscard]]
+    constexpr bool inType() const noexcept { return _ctx.inType; }
+
+    [[nodiscard]]
+    constexpr bool inModule() const noexcept { return _ctx.inModule; }
+
+    void visitLoop(BlockExpr* body);
+    void visitFn(Expr* body);
+
+    void error(Location start, Location end, std::string message, std::vector<Diagnostic> subs = {}) {
+        _diagnostics.add(Diagnostic::error(start, end, std::move(message), std::move(subs)));
+    }
+};
+
+} // Spark::FrontEnd
