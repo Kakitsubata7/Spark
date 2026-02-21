c365ab2c401f63a99e463e9e5805aaf3d34fc866
Dongxin Tian <70252050+kakitsubata7@users.noreply.github.com>
2026-02-21 00:09:58 -0500

    SymbolKind

--- src/frontend/semantic/symbol.hpp
+++ src/frontend/semantic/symbol.hpp
@@ -1,19 +1,42 @@
 ﻿#pragma once
 
+#include <memory>
+#include <vector>
+
 #include "frontend/ast/node.hpp"
+#include "symbol.hpp"
 
 namespace Spark::FrontEnd {
 
+enum class SymbolKind {
+    Var, Func, Type, Module
+};
+
 struct Symbol {
     /**
-     * Declaration `Name` node.
+     * Declaration kind of the symbol.
      */
-    Name* name;
+    SymbolKind kind;
 
     /**
      * Whether the symbol is reassignable or not.
      */
     bool isReassignable;
+
+    /**
+     * Declaration `Name` node.
+     */
+    Name* name;
+};
+
+class SymbolTable {
+private:
+    std::vector<std::unique_ptr<Symbol>> _symbols;
+
+public:
+    Symbol* make(Symbol symbol) {
+        return _symbols.emplace_back(std::make_unique<Symbol>(symbol)).get();
+    }
 };
 
 } // Spark::FrontEnd
