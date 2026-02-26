9571f91db7bd81a8fb9e28fbc1d94828d95bd6d5
Dongxin Tian <70252050+kakitsubata7@users.noreply.github.com>
2026-02-26 15:56:45 -0500

    Env

---
+++ src/frontend/semantic/env.hpp
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+#include <string_view>
+#include <unordered_map>
+
+#include "symbol.hpp"
+
+namespace Spark::FrontEnd {
+
+/**
+ * Represents a scope's binding environment, mapping name to symbols declared in the environment.
+ */
+class Env {
+private:
+    std::unordered_map<std::string_view, Symbol*> _map;
+
+public:
+    Symbol* get(std::string_view name) const {
+        auto it = _map.find(name);
+        return it != _map.end() ? it->second : nullptr;
+    }
+
+    void set(std::string_view name, Symbol* symbol) {
+        _map.emplace(name, symbol);
+    }
+
+    void remove(std::string_view name) {
+        _map.erase(name);
+    }
+};
+
+} // Spark::FrontEnd
