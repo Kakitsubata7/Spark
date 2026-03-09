8b0479aa541c88d1d1682e275fbea82f68010d95
Dongxin Tian <70252050+kakitsubata7@users.noreply.github.com>
2026-03-09 13:51:41 -0400

    wip

--- src/frontend/semantic/name_resolver.cpp
+++ src/frontend/semantic/name_resolver.cpp
@@ -4,93 +4,45 @@
 
 namespace Spark::FrontEnd {
 
-static Diagnostic redeclareError(Location start, Location end, const Symbol* symbol) {
-    std::ostringstream msg;
-    msg << "redeclaration of `" << symbol->name() << "`";
+void PatternDeclarator::visit(Name* node) {
+    assert(node != nullptr);
 
-    std::ostringstream note;
-    note << "previously declared as a ";
-    switch (symbol->kind) {
-        case SymbolKind::Var:
-            note << "variable";
-            break;
-        case SymbolKind::Func:
-            note << "function";
-            break;
-        case SymbolKind::Type:
-            note << "type";
-            break;
-        case SymbolKind::Module:
-            note << "module";
-            break;
-    }
-    note << " here";
+    // Checks if the name is already declared locally in the environment
+    std::string_view name = node->value.str();
+    Symbol* symbol = _env.get(name);
+    if (symbol == nullptr) {
+        // Create symbol
+        symbol = _symbolTable.make(Symbol{
+            .kind = _kind,
+            .isReassignable = _isReassignable,
+            .node = node
+        });
 
-    return Diagnostic::error(start, end, msg.str(), {
-        Diagnostic::note(symbol->start(), symbol->end(), note.str())
-    });
-}
-
-static Diagnostic cannotFindError(Location start,
-                                  Location end,
-                                  std::string_view name,
-                                  const std::optional<std::string_view>& base) {
-    std::ostringstream oss;
-    oss << "cannot find name `" << name << "`";
-    if (base.has_value()) {
-        oss << " under `" << base.value() << "`";
-    }
-    return Diagnostic::error(start, end, oss.str());
-}
-
-
-
-void Declarator::declare(Name* name,
-                         SymbolTable& symbolTable,
-                         Env& env,
-                         SymbolKind kind,
-                         bool isReassignable,
-                         Diagnostics& diagnostics) {
-    Declarator declarator{symbolTable, env, kind, isReassignable, diagnostics};
-    name->accept(declarator);
-}
-
-void Declarator::declare(Pattern* pattern,
-                         SymbolTable& symbolTable,
-                         Env& env,
-                         SymbolKind kind,
-                         bool isReassignable,
-                         Diagnostics& diagnostics) {
-    Declarator declarator{symbolTable, env, kind, isReassignable, diagnostics};
-    pattern->accept(declarator);
-}
-
-void Declarator::visit(Name* name) {
-    // Checks if the name already exists in the environment
-    if (Symbol* symbol = _env.get(name->value.str()); symbol != nullptr) {
-        diagnostic(redeclareError(name->start, name->end, symbol));
-        return;
+        // Bind symbol to environment
+        _env.set(name, symbol);
+    } else {
+        // Redeclaration error, reuse the previously declared symbol
+        redeclareError(node, symbol->node);
     }
 
-    // Creates symbol and declares name
-    _env.set(name->value.str(), _symbolTable.make(Symbol{
-        .kind = _kind,
-        .isReassignable = _isReassignable,
-        .node = name
-    }));
+    // Bind symbol to the `Name` node
+    _nodeSymbolMap.set(node, symbol);
 }
 
-void Declarator::visit(BindingPattern* pattern) {
+void PatternDeclarator::visit(BindingPattern* pattern) {
+    assert(pattern != nullptr);
     pattern->name->accept(*this);
 }
 
-void Declarator::visit(TuplePattern* pattern) {
+void PatternDeclarator::visit(TuplePattern* pattern) {
+    assert(pattern != nullptr);
     for (Pattern* p : pattern->patterns) {
         p->accept(*this);
     }
 }
 
-void Declarator::visit(CollectionPattern* pattern) {
+void PatternDeclarator::visit(CollectionPattern* pattern) {
+    assert(pattern != nullptr);
     for (Pattern* p : pattern->prefix) {
         p->accept(*this);
     }
@@ -99,342 +51,96 @@ void Declarator::visit(CollectionPattern* pattern) {
     }
 }
 
-void Declarator::visit(RecordPattern* pattern) {
+void PatternDeclarator::visit(RecordPattern* pattern) {
+    assert(pattern != nullptr);
     for (RecordPatternField* field : pattern->fields) {
         field->pattern->accept(*this);
     }
 }
 
+void PatternDeclarator::redeclareError(const Name* node, const Name* prevDeclareNode) noexcept {
+    assert(node != nullptr);
+    assert(prevDeclareNode != nullptr);
+
+    std::ostringstream msg;
+    std::string_view name = node->value.str();
+    msg << "redeclaration of name: `" << name << "`";
+    _diagnostics.add(Diagnostic::error(node->start, node->end, msg.str(), {
+        Diagnostic::note(prevDeclareNode->start, node->end, "previously declared here")
+    }));
+}
 
 
-void StmtDeclarator::declare(Stmt* stmt, NameResolveContext& ctx, Env& env, Diagnostics& diagnostics) {
-    if (stmt == nullptr) {
+
+void NameDeclarator::visit(VarDefStmt* vardef) {
+    assert(vardef != nullptr);
+    bool isReassignable = vardef->mod->kind == VarModifier::VarKind::Let;
+    PatternDeclarator declarator{_symbolTable, _nodeSymbolMap, _env, SymbolKind::Var, isReassignable, _diagnostics};
+    vardef->pattern->accept(declarator);
+}
+
+void NameDeclarator::visit(FnDefStmt* fndef) {
+    assert(fndef != nullptr);
+    PatternDeclarator declarator{_symbolTable, _nodeSymbolMap, _env, SymbolKind::Func, false, _diagnostics};
+    fndef->name->accept(declarator);
+}
+
+
+
+void NameResolver::visit(NameExpr* ident) {
+    std::string_view name = ident->name->value.str();
+    if (Symbol* symbol = _env.find(name); symbol != nullptr) {
+        _nodeSymbolMap.set(ident, symbol);
+        _nodeSymbolMap.set(ident->name, symbol);
         return;
     }
-
-    StmtDeclarator declarator{ctx, env, diagnostics};
-    stmt->accept(declarator);
+    cannotFindError(ident->start, ident->end, name);
 }
 
-void StmtDeclarator::visit(VarDefStmt* vardef) {
-    Declarator::declare(vardef->pattern, _ctx.symbolTable(), _env, SymbolKind::Var,
-        vardef->mod->kind == VarModifier::VarKind::Let, _diagnostics);
-}
-
-void StmtDeclarator::visit(FnDefStmt* fndef) {
-    Declarator::declare(fndef->name, _ctx.symbolTable(), _env, SymbolKind::Func, false,
-        _diagnostics);
-}
-
-void StmtDeclarator::visit(TypeDefStmt* tdef) {
-    Declarator::declare(tdef->name, _ctx.symbolTable(), _env, SymbolKind::Type, false,
-        _diagnostics);
-}
-
-void StmtDeclarator::visit(ModuleStmt* mdef) {
-    std::vector<PathSeg*>& segs = mdef->path->segs;
-
-    Env* currentEnv = &_env;
-
-    for (PathSeg* seg : segs) {
-        Name* name = seg->name;
-        Symbol* symbol = currentEnv->get(name->value.str());
-
-        if (symbol != nullptr) {
-            // Checks if the name was declared with a different kind
-            if (symbol->kind != SymbolKind::Module) {
-                diagnostic(redeclareError(name->start, name->end, symbol));
-                return;
-            }
-        } else {
-            // Creates a new symbol and bind it under the current env
-            symbol = _ctx.makeSymbol(Symbol{
-                .kind = SymbolKind::Module,
-                .isReassignable = false,
-                .node = name
-            });
-            currentEnv->set(name->value.str(), symbol);
-        }
-
-        currentEnv = &_ctx.getOrCreateSubEnv(symbol);
-    }
-
-    // Declares exported names in the module to the last symbol in the path
-    for (Node* node : mdef->body->nodes) {
-        if (auto* e = node->as<ExportStmt>()) {
-            declare(e->stmt, _ctx, *currentEnv, _diagnostics);
-        }
-    }
+void NameResolver::cannotFindError(Location start, Location end, std::string_view name) {
+    std::ostringstream msg;
+    msg << "cannot find name: `" << name << "`";
+    _diagnostics.add(Diagnostic::error(start, end, msg.str()));
 }
 
 
 
-void GlobalNameResolver::resolve(const AST& ast, NameResolveContext& ctx, Diagnostics& diagnostics) {
-    if (ast.root == nullptr) {
-        return;
-    }
-
-    GlobalNameResolver resolver{ctx, diagnostics};
-    if (auto* block = ast.root->as<BlockExpr>()) {
-        for (Node* node : block->nodes) {
-            node->accept(resolver);
-        }
-    } else {
-        ast.root->accept(resolver);
-    }
-}
-
-void GlobalNameResolver::visit(ExportStmt* e) {
-    StmtDeclarator::declare(e->stmt, _ctx, _ctx.globalEnv(), _diagnostics);
-}
-
-
-
-void NameResolver::resolve(const AST& ast, NameResolveContext& ctx, Diagnostics& diagnostics) {
-    if (ast.root == nullptr) {
-        return;
-    }
-
-    NameResolver resolver{ctx, diagnostics};
-    ast.root->accept(resolver);
-}
-
-void NameResolver::visit(LambdaExpr* expr) {
-
-}
-
-void NameResolver::visit(IfThenExpr* expr) {
-    expr->condition->accept(*this);
-    expr->thenExpr->accept(*this);
-    expr->elseExpr->accept(*this);
-}
-
-void NameResolver::visit(TryElseExpr* expr) {
-    expr->tryExpr->accept(*this);
-    expr->elseExpr->accept(*this);
-}
-
-void NameResolver::visit(MatchExpr* expr) {
-    // Resolves scrutinee
-    expr->scrutinee->accept(*this);
-
-    for (MatchCase* c : expr->cases) {
-        Env& env = pushEnv();
-
-        // Declares case binding pattern
-        if (c->pattern != nullptr) {
-            Declarator::declare(c->pattern, _ctx.symbolTable(), env, SymbolKind::Var, false,
-                _diagnostics);
-        }
-
-        // Resolves guard
-        if (c->guard != nullptr) {
-            c->guard->accept(*this);
-        }
-
-        // Resolves body
-        pushEnv();
-        c->body->accept(*this);
-        popEnv();
-
-        popEnv();
-    }
-}
-
-void NameResolver::visit(TryCatchExpr* expr) {
-    // Resolves try body
-    expr->expr->accept(*this);
-
-    // Resolve catch clauses
-    for (CatchClause* c : expr->catches) {
-        pushEnv();
-        c->body->accept(*this);
-        popEnv();
-    }
-}
-
-void NameResolver::visit(ThrowExpr* expr) {
-    expr->expr->accept(*this);
-}
-
-void NameResolver::visit(BlockExpr* expr) {
+void SemanticResolver::visit(BlockExpr* block) {
     pushEnv();
-    for (Node* node : expr->nodes) {
-        node->accept(*this);
-    }
-    popEnv();
-}
 
-void NameResolver::visit(IsExpr* expr) {
-    expr->expr->accept(*this);
-    expr->type->accept(*this);
-}
+    // Create name declarator
+    NameDeclarator nameDeclarator{};
 
-void NameResolver::visit(AsExpr* expr) {
-    // expr->expr->accept(*this);
-    // Declarator::declare(expr->pattern, _ctx.symbolTable(), currentEnv(), SymbolKind::Var, false,
-    //     _diagnostics);
-    // expr->type->accept(*this);
-    std::cerr << "`as` is not supported" << std::endl;
-    abort();
-}
-
-void NameResolver::visit(BinaryExpr* expr) {
-    expr->lhs->accept(*this);
-    expr->rhs->accept(*this);
-}
-
-void NameResolver::visit(PrefixExpr* expr) {
-    expr->expr->accept(*this);
-}
-
-void NameResolver::visit(PostfixExpr* expr) {
-    expr->expr->accept(*this);
-}
-
-void NameResolver::visit(CallExpr* expr) {
-    expr->callee->accept(*this);
-    for (CallArg* arg : expr->args) {
-        arg->expr->accept(*this);
-    }
-}
-
-void NameResolver::visit(SubscriptExpr* expr) {
-    expr->base->accept(*this);
-    for (Expr* index : expr->indices) {
-        index->accept(*this);
-    }
-}
-
-void NameResolver::visit(NameExpr* expr) {
-    resolveName(expr->name, currentEnv());
-}
-
-void NameResolver::visit(GlobalAccessExpr* expr) {
-    resolveName(expr->name, _ctx.globalEnv(), "global");
-}
-
-void NameResolver::visit(UpvalueExpr* expr) {
-    std::cerr << "`$` is not supported" << std::endl;
-    abort();
-}
-
-void NameResolver::visit(MemberAccessExpr* expr) {
-    // Resolves base expression
-    expr->base->accept(*this);
-
-    // TODO
-}
-
-void NameResolver::visit(TupleExpr* expr) {
-    for (Expr* e : expr->exprs) {
-        e->accept(*this);
-    }
-}
-
-void NameResolver::visit(CollectionExpr* expr) {
-    for (Expr* e : expr->exprs) {
-        e->accept(*this);
-    }
-}
-
-void NameResolver::visit(TypeofExpr* expr) {
-    expr->expr->accept(*this);
-}
-
-void NameResolver::visit(VarDefStmt* stmt) {
-
-}
-
-void NameResolver::visit(FnDefStmt* stmt) {
-
-}
-
-void NameResolver::visit(TypeDefStmt* stmt) {
-
-}
-
-void NameResolver::visit(CaseDefStmt* stmt) {
-    std::cerr << "enum member and ADT member are not supported" << std::endl;
-    abort();
-}
-
-void NameResolver::visit(AssignStmt* stmt) {
-    stmt->lhs->accept(*this);
-    stmt->rhs->accept(*this);
-}
-
-void NameResolver::visit(IfStmt* stmt) {
-    stmt->condition->accept(*this);
-    stmt->thenBody->accept(*this);
-    if (stmt->elseBody != nullptr) {
-        stmt->elseBody->accept(*this);
-    }
-}
-
-void NameResolver::visit(WhileStmt* stmt) {
-    stmt->condition->accept(*this);
-    stmt->body->accept(*this);
-}
-
-void NameResolver::visit(DoWhileStmt* stmt) {
-    stmt->body->accept(*this);
-    stmt->condition->accept(*this);
-}
-
-void NameResolver::visit(ForStmt* stmt) {
-    // Resolves range
-    stmt->range->accept(*this);
-
-    Env& forEnv = pushEnv();
-
-    // Declares iterator
-    Declarator::declare(stmt->iterator, _ctx.symbolTable(), forEnv, SymbolKind::Var, false,
-        _diagnostics);
-
-    // Resolves body
-    stmt->body->accept(*this);
-
-    popEnv();
-}
-
-void NameResolver::visit(ReturnStmt* stmt) {
-    if (stmt->expr != nullptr) {
-        stmt->expr->accept(*this);
-    }
-}
-
-void NameResolver::visit(ModuleStmt* stmt) {
-    StmtDeclarator::declare(stmt, _ctx, currentEnv(), _diagnostics);
-    // TODO
-}
-
-void NameResolver::visit(ExportStmt* stmt) {
-
-}
-
-void NameResolver::visit(UndefineStmt* stmt) {
-    Env* env = &currentEnv();
-    std::optional<std::string_view> base = std::nullopt;
-    for (PathSeg* seg : stmt->path->segs) {
-        // Check if previous name has a sub-environment, if not, it means the symbol cannot be found under that name
-        if (env == nullptr) {
-            diagnostic(cannotFindError(seg->name->start, seg->name->end, seg->name->value.str(), base));
-            return;
+    // Declare hoisted names
+    for (Node* node : block->nodes) {
+        if (isHoistedDeclarative(node)) {
+            node->accept(nameDeclarator);
         }
+    }
 
-        Symbol* symbol = resolveName(seg->name, *env, base);
-        env = (symbol != nullptr && _ctx.hasSubEnv(symbol)) ? &_ctx.getSubEnv(symbol) : nullptr;
-        base = seg->name->value.str();
+    // Resolves names
+    NameResolver nameResolver{};
+
+    popEnv();
+}
+
+void SemanticResolver::resolveTopLevel(Node* node) {
+    assert(node != nullptr);
+
+    std::vector<Node*> nodes;
+    if (auto* block = node->as<BlockExpr>()) {
+        nodes = block->nodes;
+    } else {
+        nodes.push_back(node);
     }
 }
 
-Symbol* NameResolver::resolveName(const Name* name, const Env& env, const std::optional<std::string_view>& base) {
-    Symbol* symbol = env.get(name->value.str());
-    if (symbol == nullptr) {
-        diagnostic(cannotFindError(name->start, name->end, name->value.str(), base));
-        return nullptr;
+bool SemanticResolver::isHoistedDeclarative(const Node* node) noexcept {
+    assert(node != nullptr);
+
+    if (node->is<FnDefStmt>()) {
+        return true;
     }
-    return symbol;
+    return false;
 }
 } // Spark::FrontEnd
--- src/frontend/semantic/name_resolver.hpp
+++ src/frontend/semantic/name_resolver.hpp
@@ -1,254 +1,128 @@
 ﻿#pragma once
 
 #include <string_view>
-#include <utility>
-#include <vector>
 
+#include "env.hpp"
 #include "frontend/ast.hpp"
+#include "semantic_visitor.hpp"
 #include "symbol.hpp"
 #include "utils/diagnostic.hpp"
 
 namespace Spark::FrontEnd {
 
-/**
- * Represents an environment that maps names to symbols during name resolution.
- */
-class Env {
-private:
-    std::unordered_map<std::string, Symbol*> _map;
-
-public:
-    void set(std::string_view name, Symbol* symbol) {
-        _map.emplace(std::string(name), symbol);
-    }
-
-    Symbol* get(std::string_view name) const {
-        auto it = _map.find(std::string{name});
-        return it == _map.end() ? nullptr : it->second;
-    }
-};
-
-/**
- * Represents name resolution state context.
- * It stores the global environment and maps symbols to their sub-environment if they have one.
- */
-class NameResolveContext {
+class PatternDeclarator : public NodeVisitor {
 private:
     SymbolTable& _symbolTable;
+    NodeSymbolMap& _nodeSymbolMap;
 
-    Env _globalEnv;
-
-    std::unordered_map<const Symbol*, std::unique_ptr<Env>> _subEnvMap;
-
-public:
-    explicit NameResolveContext(SymbolTable& symbolTable) noexcept : _symbolTable(symbolTable) { }
-
-    [[nodiscard]]
-    SymbolTable& symbolTable() const noexcept { return _symbolTable; }
-
-    [[nodiscard]]
-    Env& globalEnv() noexcept { return _globalEnv; }
-
-    Symbol* makeSymbol(Symbol symbol) {
-        return _symbolTable.make(symbol);
-    }
-
-    Env& getOrCreateSubEnv(const Symbol* symbol) {
-        assert(symbol != nullptr);
-
-        auto it = _subEnvMap.find(symbol);
-        if (it == _subEnvMap.end()) {
-            return *_subEnvMap.emplace(symbol, std::make_unique<Env>()).first->second;
-        }
-        return *it->second;
-    }
-
-    Env& getSubEnv(const Symbol* symbol) {
-        assert(symbol != nullptr);
-
-        auto it = _subEnvMap.find(symbol);
-        assert(it != _subEnvMap.end());
-        return *it->second;
-    }
-
-    [[nodiscard]]
-    bool hasSubEnv(const Symbol* symbol) {
-        assert(symbol != nullptr);
-        return _subEnvMap.find(symbol) != _subEnvMap.end();
-    }
-};
-
-class Declarator : public NodeVisitor {
-private:
-    SymbolTable& _symbolTable;
     Env& _env;
+
     SymbolKind _kind;
     bool _isReassignable;
+
     Diagnostics& _diagnostics;
 
 public:
-    Declarator(SymbolTable& symbolTable,
+    PatternDeclarator(SymbolTable& symbolTable,
+                      NodeSymbolMap& nodeSymbolMap,
                       Env& env,
                       SymbolKind kind,
                       bool isReassignable,
                       Diagnostics& diagnostics) noexcept
-        : _symbolTable(symbolTable), _env(env), _kind(kind), _isReassignable(isReassignable),
-          _diagnostics(diagnostics) { }
+        : _symbolTable(symbolTable), _nodeSymbolMap(nodeSymbolMap), _env(env), _kind(kind),
+          _isReassignable(isReassignable), _diagnostics(diagnostics) { }
 
-    static void declare(Name* name,
-                        SymbolTable& symbolTable,
-                        Env& env,
-                        SymbolKind kind,
-                        bool isReassignable,
-                        Diagnostics& diagnostics);
-
-    static void declare(Pattern* pattern,
-                        SymbolTable& symbolTable,
-                        Env& env,
-                        SymbolKind kind,
-                        bool isReassignable,
-                        Diagnostics& diagnostics);
-
-    void visit(Name* name) override;
+    void visit(Name* node) override;
     void visit(BindingPattern* pattern) override;
     void visit(TuplePattern* pattern) override;
     void visit(CollectionPattern* pattern) override;
     void visit(RecordPattern* pattern) override;
 
 private:
-    void diagnostic(Diagnostic diagnostic) {
-        _diagnostics.add(std::move(diagnostic));
-    }
+    void redeclareError(const Name* node, const Name* prevDeclareNode) noexcept;
 };
 
-class StmtDeclarator : public NodeVisitor {
+class NameDeclarator : public NodeVisitor {
 private:
-    NameResolveContext& _ctx;
+    SymbolTable& _symbolTable;
+    NodeSymbolMap& _nodeSymbolMap;
+
     Env& _env;
+
     Diagnostics& _diagnostics;
 
 public:
-    StmtDeclarator(NameResolveContext& ctx, Env& env, Diagnostics& diagnostics) noexcept
-        : _ctx(ctx), _env(env), _diagnostics(diagnostics) { }
-
-    static void declare(Stmt* stmt, NameResolveContext& ctx, Env& env, Diagnostics& diagnostics);
+    NameDeclarator(SymbolTable& symbolTable,
+                   NodeSymbolMap& nodeSymbolMap,
+                   Env& env,
+                   Diagnostics& diagnostics) noexcept
+        : _symbolTable(symbolTable), _nodeSymbolMap(nodeSymbolMap), _env(env), _diagnostics(diagnostics) { }
 
     void visit(VarDefStmt* vardef) override;
     void visit(FnDefStmt* fndef) override;
-    void visit(TypeDefStmt* tdef) override;
-    void visit(ModuleStmt* mdef) override;
-
-private:
-    void diagnostic(Diagnostic diagnostic) {
-        _diagnostics.add(std::move(diagnostic));
-    }
 };
 
-/**
- * Resolves all globally exported names to symbols and add them to the global environment.
- * If the name already exists in the given global environment, it will be overridden.
- */
-class GlobalNameResolver : public NodeVisitor {
+
+
+class NameResolver : public NodeVisitor {
 private:
-    NameResolveContext& _ctx;
+    SymbolTable& _symbolTable;
+    NodeSymbolMap& _nodeSymbolMap;
+    const Env& _env;
     Diagnostics& _diagnostics;
 
 public:
-    GlobalNameResolver(NameResolveContext& ctx, Diagnostics& diagnostics) noexcept
-        : _ctx(ctx), _diagnostics(diagnostics) { }
+    NameResolver(SymbolTable& symbolTable, NodeSymbolMap& nodeSymbolMap, Env& env, Diagnostics& diagnostics) noexcept
+        : _symbolTable(symbolTable), _nodeSymbolMap(nodeSymbolMap), _env(env), _diagnostics(diagnostics) { }
 
-    static void resolve(const AST& ast, NameResolveContext& ctx, Diagnostics& diagnostics);
+    void visit(NameExpr* ident) override;
 
-    void visit(ExportStmt* e) override;
+    void cannotFindError(Location start, Location end, std::string_view name);
 };
 
-/**
- * Resolves all lexical names and module names to symbols, ignoring globally exported names.
- */
-class NameResolver : public NodeVisitor {
-private:
-    NameResolveContext& _ctx;
-    Diagnostics& _diagnostics;
 
+
+class SemanticResolver : public SemanticVisitor {
+private:
+    Env& _globalEnv;
     std::vector<Env> _envStack;
 
 public:
-    explicit NameResolver(NameResolveContext& ctx, Diagnostics& diagnostics) noexcept
-        : _ctx(ctx), _diagnostics(diagnostics) { }
+    SemanticResolver(SemanticContext& ctx, Env& globalEnv, Diagnostics& diagnostics)
+        : SemanticVisitor(ctx, diagnostics), _globalEnv(globalEnv) { }
 
-    static void resolve(const AST& ast, NameResolveContext& ctx, Diagnostics& diagnostics);
-
-    void visit(LambdaExpr* expr) override;
-    void visit(IfThenExpr* expr) override;
-    void visit(TryElseExpr* expr) override;
-    void visit(MatchExpr* expr) override;
-    void visit(TryCatchExpr* expr) override;
-    void visit(ThrowExpr* expr) override;
-    void visit(BlockExpr* expr) override;
-    void visit(IsExpr* expr) override;
-    void visit(AsExpr* expr) override;
-    void visit(BinaryExpr* expr) override;
-    void visit(PrefixExpr* expr) override;
-    void visit(PostfixExpr* expr) override;
-    void visit(MemberAccessExpr* expr) override;
-    void visit(CallExpr* expr) override;
-    void visit(SubscriptExpr* expr) override;
-    void visit(NameExpr* expr) override;
-    void visit(GlobalAccessExpr* expr) override;
-    void visit(UpvalueExpr* expr) override;
-    void visit(TupleExpr* expr) override;
-    void visit(CollectionExpr* expr) override;
-    void visit(TypeofExpr* expr) override;
-
-    void visit(VarDefStmt* stmt) override;
-    void visit(FnDefStmt* stmt) override;
-    void visit(TypeDefStmt* stmt) override;
-    void visit(CaseDefStmt* stmt) override;
-    void visit(AssignStmt* stmt) override;
-    void visit(IfStmt* stmt) override;
-    void visit(WhileStmt* stmt) override;
-    void visit(DoWhileStmt* stmt) override;
-    void visit(ForStmt* stmt) override;
-    void visit(ReturnStmt* stmt) override;
-    void visit(ModuleStmt* stmt) override;
-    void visit(ExportStmt* stmt) override;
-    void visit(ImportStmt* stmt) override;
-    void visit(ImportAllStmt* stmt) override;
-    void visit(UndefineStmt* stmt) override;
+    static void resolve(const AST& ast, SemanticContext& ctx, Env& globalEnv, Diagnostics& diagnostics) {
+        if (ast.root == nullptr) {
+            return;
+        }
+        SemanticResolver resolver{ctx, globalEnv, diagnostics};
+        resolver.resolveTopLevel(ast.root);
+    }
 
 private:
-    Env& pushEnv() {
-        return _envStack.emplace_back();
+    void visit(BlockExpr* block) override;
+
+    Env& currentEnv() noexcept {
+        return _envStack.empty() ? _globalEnv : _envStack.back();
+    }
+
+    void pushEnv() {
+        _envStack.emplace_back(currentEnv());
     }
 
     void popEnv() {
-        if (!_envStack.empty()) {
-            _envStack.pop_back();
-        }
-    }
-
-    [[nodiscard]]
-    Env& currentEnv() {
         assert(!_envStack.empty());
-        return _envStack.back();
+        _envStack.pop_back();
     }
 
-    [[nodiscard]]
-    Symbol* find(std::string_view name) {
-        for (auto it = _envStack.rbegin(); it != _envStack.rend(); ++it) {
-            Env& env = *it;
-            if (Symbol* symbol = env.get(name)) {
-                return symbol;
-            }
-        }
-        return _ctx.globalEnv().get(name);
+    void resolve(Node* node) {
+        assert(node != nullptr);
+        node->accept(*this);
     }
 
-    Symbol* resolveName(const Name* name, const Env& env, const std::optional<std::string_view>& base = std::nullopt);
+    void resolveTopLevel(Node* node);
 
-    void diagnostic(Diagnostic diagnostic) {
-        _diagnostics.add(std::move(diagnostic));
-    }
+    static bool isHoistedDeclarative(const Node* node) noexcept;
 };
 
 } // Spark::FrontEnd
