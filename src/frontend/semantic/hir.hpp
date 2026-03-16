ae57b095cab6aa11f6755213c643aa769beff1e5
Dongxin Tian <70252050+kakitsubata7@users.noreply.github.com>
2026-03-16 15:07:26 -0400

    Update hir.hpp

--- src/frontend/semantic/hir.hpp
+++ src/frontend/semantic/hir.hpp
@@ -13,52 +13,64 @@ struct HIRExpr : HIRNode {
 
 };
 
-struct HIRBlock : HIRExpr {
+struct HIRBlock final : HIRExpr {
     std::vector<HIRNode*> nodes;
 
     explicit HIRBlock(std::vector<HIRNode*> nodes) : nodes(std::move(nodes)) { }
 };
 
-struct HIRCall : HIRExpr {
+struct HIRCall final : HIRExpr {
+    HIRExpr* callee;
+    std::vector<HIRExpr*> args;
+
+    HIRCall(HIRExpr* callee, std::vector<HIRExpr*> args) : callee(callee), args(std::move(args)) { }
+};
+
+struct HIRFieldAccess final : HIRExpr {
 
 };
 
-struct HIRFieldAccess : HIRExpr {
-
-};
-
-struct HIRIntLiteral : HIRExpr {
+struct HIRIntLiteral final : HIRExpr {
     BigInt value;
 
     explicit HIRIntLiteral(BigInt value) : value(std::move(value)) { }
 };
 
-struct HIRRealLiteral : HIRExpr {
+struct HIRRealLiteral final : HIRExpr {
     BigReal value;
 
     explicit HIRRealLiteral(BigReal value) : value(std::move(value)) { }
 };
 
-struct HIRStringLiteral : HIRExpr {
+struct HIRStringLiteral final : HIRExpr {
     std::string value;
 
     explicit HIRStringLiteral(std::string value) : value(std::move(value)) { }
 };
 
-struct HIRBoolLiteral : HIRExpr {
+struct HIRBoolLiteral final : HIRExpr {
     bool value;
 
     explicit HIRBoolLiteral(bool value) : value(value) { }
 };
 
-struct HIRNilLiteral : HIRExpr { };
+struct HIRNilLiteral final : HIRExpr { };
 
-struct HIRVoidLiteral : HIRExpr { };
+struct HIRVoidLiteral final : HIRExpr { };
 
-struct HIRAssign : HIRNode {
+struct HIRAssign final : HIRNode {
     HIRExpr* expr;
 
     explicit HIRAssign(HIRExpr* expr) : expr(expr) { }
+};
+
+struct HIRIf final : HIRExpr {
+    HIRExpr* condition;
+    HIRExpr* then;
+    HIRExpr* els;
+
+    HIRIf(HIRExpr* cond, HIRExpr* then, HIRExpr* els) noexcept
+        : condition(cond), then(then), els(els) { }
 };
 
 class HIR {
