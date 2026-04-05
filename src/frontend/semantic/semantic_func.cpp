bde73a63901c73eb4c1f180a101b32b1a4293722
Dongxin Tian <70252050+kakitsubata7@users.noreply.github.com>
2026-04-05 05:53:11 -0400

    Updated `SemanticFunc`

--- src/frontend/semantic/semantic_func.cpp
+++ src/frontend/semantic/semantic_func.cpp
@@ -1,7 +1,37 @@
 #include "semantic_func.hpp"
 
+#include <boost/functional/hash.hpp>
+
+#include "semantic_type.hpp"
+
 namespace Spark::FrontEnd {
 
+bool FuncSignature::operator==(const FuncSignature& other) const noexcept {
+    if (this == &other) {
+        return true;
+    }
 
+    // Check parameter types
+    if (_paramTypes.size() != other._paramTypes.size()) {
+        return false;
+    }
+    for (size_t i = 0; i < _paramTypes.size(); i++) {
+        if (!_paramTypes[i]->isIdentical(other._paramTypes[i])) {
+            return false;
+        }
+    }
+
+    // Check return type
+    return _returnType->isIdentical(other._returnType);
+}
+
+size_t FuncSignatureHash::operator()(const FuncSignature& sig) const noexcept {
+    size_t seed = 0;
+    for (auto* t : sig.paramTypes()) {
+        boost::hash_combine(seed, t->id());
+    }
+    boost::hash_combine(seed, sig.returnType()->id());
+    return seed;
+}
 
 } // Spark::FrontEnd
--- src/frontend/semantic/semantic_func.hpp
+++ src/frontend/semantic/semantic_func.hpp
@@ -9,23 +9,58 @@ namespace Spark::FrontEnd {
 class SemanticType;
 
 /**
- * Represents a function during semantic passes.
+ * Represents a semantic function's signature
  */
-class SemanticFunc {
+class FuncSignature {
 private:
     std::vector<SemanticType*> _paramTypes;
-    std::vector<SemanticType*> _returnTypes;
+    SemanticType* _returnType;
 
 public:
-    SemanticFunc(std::vector<SemanticType*> paramTypes,
-                 std::vector<SemanticType*> returnTypes) noexcept
-        : _paramTypes(std::move(paramTypes)), _returnTypes(std::move(returnTypes)) { }
+    FuncSignature(std::vector<SemanticType*> paramTypes, SemanticType* returnType) noexcept
+        : _paramTypes(std::move(paramTypes)), _returnType(returnType) { }
 
     [[nodiscard]]
     const std::vector<SemanticType*>& paramTypes() const noexcept { return _paramTypes; }
 
     [[nodiscard]]
-    const std::vector<SemanticType*>& returnTypes() const noexcept { return _returnTypes; }
+    SemanticType* returnType() const noexcept { return _returnType; }
+
+    bool operator==(const FuncSignature& other) const noexcept;
+};
+
+/**
+ * Represents the hasher for `FuncSignature`.
+ */
+struct FuncSignatureHash {
+    size_t operator()(const FuncSignature& sig) const noexcept;
+};
+
+/**
+ * Represents a function during semantic passes.
+ */
+class SemanticFunc {
+private:
+    std::vector<SemanticType*> _paramTypes;
+    SemanticType* _returnType;
+
+public:
+    SemanticFunc(std::vector<SemanticType*> paramTypes,
+                 SemanticType* returnType) noexcept
+        : _paramTypes(std::move(paramTypes)), _returnType(returnType) { }
+
+    [[nodiscard]]
+    const std::vector<SemanticType*>& paramTypes() const noexcept { return _paramTypes; }
+
+    [[nodiscard]]
+    SemanticType* returnTypes() const noexcept { return _returnType; }
 };
 
 } // Spark::FrontEnd
+
+template <>
+struct std::hash<Spark::FrontEnd::FuncSignature> {
+    std::size_t operator()(const Spark::FrontEnd::FuncSignature& sig) const noexcept {
+        return Spark::FrontEnd::FuncSignatureHash{}(sig);
+    }
+};
