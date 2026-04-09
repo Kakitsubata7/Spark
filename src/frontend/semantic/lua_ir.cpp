d4e7fce2de2fef13d0731293f613da6da25931b9
Dongxin Tian <70252050+kakitsubata7@users.noreply.github.com>
2026-04-08 22:05:17 -0400

    lua_ir

---
+++ src/frontend/semantic/lua_ir.cpp
@@ -0,0 +1,132 @@
+﻿#include "lua_ir.hpp"
+
+#include <cassert>
+#include <iomanip>
+#include <sstream>
+
+namespace Spark::FrontEnd {
+
+const std::string& LuaNameMangler::mangle(const Symbol* symbol) {
+    assert(symbol != nullptr);
+
+    // Check cached name
+    if (std::string* name = find(symbol)) {
+        return *name;
+    }
+
+    // Mangle new name
+    std::string name;
+    name += "v_";
+    name += getNextSymbolId();
+    return add(symbol, std::move(name));
+}
+
+const std::string& LuaNameMangler::mangle(const SemanticFunc* func) {
+    assert(func != nullptr);
+
+    // Check cached name
+    if (std::string* name = find(func)) {
+        return *name;
+    }
+
+    // Mangle new name
+    std::string name;
+    name += "f_";
+    name += getNextFuncId();
+    return add(func, std::move(name));
+}
+
+std::string* LuaNameMangler::find(const void* p) {
+    if (auto it = _map.find(p); it != _map.end()) {
+        return &it->second;
+    }
+    return nullptr;
+}
+
+const std::string& LuaNameMangler::add(const void* p, std::string name) {
+    auto [it, inserted] = _map.emplace(p, std::move(name));
+    assert(inserted);
+    return it->second;
+}
+
+uint64_t LuaNameMangler::getNextSymbolId() noexcept {
+    uint64_t current = _symbolId++;
+    assert(_symbolId > current && "`LuaNameMangler` runs out of symbol ID");
+    return current;
+}
+
+uint64_t LuaNameMangler::getNextFuncId() noexcept {
+    uint64_t current = _funcId++;
+    assert(_funcId > current && "`LuaNameMangler` runs out of function ID");
+    return current;
+}
+
+
+
+std::string LuaIntExpr::emit(LuaNameMangler& mangler) const {
+    return std::to_string(_value);
+}
+
+std::string LuaRealExpr::emit(LuaNameMangler& mangler) const {
+    std::ostringstream oss;
+    oss << std::setprecision(17) << _value;
+    return oss.str();
+}
+
+std::string LuaBoolExpr::emit(LuaNameMangler& mangler) const {
+    return _value ? "true" : "false";
+}
+
+std::string LuaStringExpr::emit(LuaNameMangler& mangler) const {
+    std::string s;
+    s += '"';
+    s += _value;
+    s += '"';
+    return s;
+}
+
+std::string LuaVoidExpr::emit(LuaNameMangler& mangler) const {
+    return "nil";
+}
+
+std::string LuaNilExpr::emit(LuaNameMangler& mangler) const {
+    return "nil";
+}
+
+std::string LuaVarExpr::emit(LuaNameMangler& mangler) const {
+    return mangler.mangle(_symbol);
+}
+
+std::string LuaFuncRefExpr::emit(LuaNameMangler& mangler) const {
+    return mangler.mangle(_func);
+}
+
+std::string LuaCallExpr::emit(LuaNameMangler& mangler) const {
+    std::ostringstream oss;
+    oss << _callee->emit(mangler) << '(';
+    for (size_t i = 0; i < _args.size(); ++i) {
+        oss << _args[i]->emit(mangler);
+        if (i != _args.size() - 1) {
+            oss << ", ";
+        }
+    }
+    oss << ')';
+    return oss.str();
+}
+
+std::string LuaMemberAccessExpr::emit(LuaNameMangler& mangler) const {
+    std::ostringstream oss;
+    std::string base = _base->emit(mangler);
+    if (auto* symbol = std::get_if<Symbol*>(&_member)) {
+        oss << base << '.' << mangler.mangle(*symbol);
+    } else if (auto* func = std::get_if<SemanticFunc*>(&_member)) {
+        oss << "(function(...) "
+                << "return " << mangler.mangle(*func) << '(' << base << ", ...)"
+            << " end)";
+    } else {
+        assert(false && "invalid `std::variant<Symbol*, SemanticFunc*>` value");
+    }
+    return oss.str();
+}
+
+} // Spark::FrontEnd
---
+++ src/frontend/semantic/lua_ir.hpp
@@ -0,0 +1,170 @@
+﻿#pragma once
+
+#include <cstdint>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+#include <variant>
+#include <vector>
+
+#include "semantic_func.hpp"
+#include "symbol.hpp"
+
+namespace Spark::FrontEnd {
+
+class LuaNameMangler {
+private:
+    std::unordered_map<const void*, std::string> _map;
+
+    uint64_t _symbolId = 0;
+    uint64_t _funcId = 0;
+
+public:
+    const std::string& mangle(const Symbol* symbol);
+    const std::string& mangle(const SemanticFunc* func);
+
+private:
+    std::string* find(const void* p);
+    const std::string& add(const void* p, std::string name);
+
+    [[nodiscard]]
+    uint64_t getNextSymbolId() noexcept;
+
+    [[nodiscard]]
+    uint64_t getNextFuncId() noexcept;
+};
+
+
+
+class LuaNode {
+public:
+    virtual ~LuaNode() = default;
+
+    [[nodiscard]]
+    virtual std::string emit(LuaNameMangler& mangler) const = 0;
+};
+
+class LuaBlock {
+
+};
+
+class LuaExpr : public LuaNode {
+public:
+    [[nodiscard]]
+    std::string emit(LuaNameMangler& mangler) const override;
+};
+
+class LuaIntExpr : public LuaExpr {
+private:
+    int64_t _value;
+
+public:
+    explicit LuaIntExpr(int64_t value) noexcept
+        : _value(value) { }
+
+    [[nodiscard]]
+    std::string emit(LuaNameMangler& mangler) const override;
+};
+
+class LuaRealExpr : public LuaExpr {
+private:
+    double _value;
+
+public:
+    explicit LuaRealExpr(double value) noexcept
+        : _value(value) { }
+
+    [[nodiscard]]
+    std::string emit(LuaNameMangler& mangler) const override;
+};
+
+class LuaBoolExpr : public LuaExpr {
+private:
+    bool _value;
+
+public:
+    explicit LuaBoolExpr(bool value) noexcept
+        : _value(value) { }
+
+    [[nodiscard]]
+    std::string emit(LuaNameMangler& mangler) const override;
+};
+
+class LuaStringExpr : public LuaExpr {
+private:
+    std::string _value;
+
+public:
+    explicit LuaStringExpr(std::string value) noexcept
+        : _value(std::move(value)) { }
+
+    [[nodiscard]]
+    std::string emit(LuaNameMangler& mangler) const override;
+};
+
+class LuaVoidExpr : public LuaExpr {
+public:
+    [[nodiscard]]
+    std::string emit(LuaNameMangler& mangler) const override;
+};
+
+class LuaNilExpr : public LuaExpr {
+public:
+    [[nodiscard]]
+    std::string emit(LuaNameMangler& mangler) const override;
+};
+
+class LuaVarExpr : public LuaExpr {
+private:
+    Symbol* _symbol;
+
+public:
+    explicit LuaVarExpr(Symbol* symbol) noexcept
+        : _symbol(symbol) { }
+
+    [[nodiscard]]
+    std::string emit(LuaNameMangler& mangler) const override;
+};
+
+class LuaFuncRefExpr : public LuaExpr {
+private:
+    SemanticFunc* _func;
+
+public:
+    explicit LuaFuncRefExpr(SemanticFunc* func) noexcept
+        : _func(func) { }
+
+    [[nodiscard]]
+    std::string emit(LuaNameMangler& mangler) const override;
+};
+
+class LuaCallExpr : public LuaExpr {
+private:
+    LuaExpr* _callee;
+    std::vector<LuaExpr*> _args;
+
+public:
+    LuaCallExpr(LuaExpr* callee, std::vector<LuaExpr*> args) noexcept
+        : _callee(callee), _args(std::move(args)) { }
+
+    [[nodiscard]]
+    std::string emit(LuaNameMangler& mangler) const override;
+};
+
+class LuaMemberAccessExpr : public LuaExpr {
+private:
+    LuaExpr* _base;
+    std::variant<Symbol*, SemanticFunc*> _member;
+
+public:
+    LuaMemberAccessExpr(LuaExpr* base, Symbol* member) noexcept
+        : _base(base), _member(member) { }
+
+    LuaMemberAccessExpr(LuaExpr* base, SemanticFunc* member) noexcept
+        : _base(base), _member(member) { }
+
+    [[nodiscard]]
+    std::string emit(LuaNameMangler& mangler) const override;
+};
+
+} // Spark::FrontEnd
