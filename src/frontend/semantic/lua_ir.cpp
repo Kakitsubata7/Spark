30a0b43c56bfdb6d496d0a3eb523800e37cbe7ef
Dongxin Tian <70252050+kakitsubata7@users.noreply.github.com>
2026-04-09 00:20:39 -0400

    Lua IR

--- src/frontend/semantic/lua_ir.cpp
+++ src/frontend/semantic/lua_ir.cpp
@@ -63,6 +63,95 @@ uint64_t LuaNameMangler::getNextFuncId() noexcept {
 
 
 
+std::string LuaFuncDef::emit(LuaNameMangler& mangler) const {
+    std::ostringstream oss;
+    oss << "function(";
+    for (size_t i = 0; i < _params.size(); ++i) {
+        oss << mangler.mangle(_params[i]);
+        if (i != _params.size() - 1) {
+            oss << ", ";
+        }
+    }
+    oss << ")\n"
+            << _body->emit(mangler)
+        << "end";
+    return oss.str();
+}
+
+std::string LuaBlock::emit(LuaNameMangler& mangler) const {
+    std::ostringstream oss;
+
+    if (!_locals.empty()) {
+        // Local variable declarations
+        emitLocalVarDecls(oss, mangler, _locals);
+        oss << '\n';
+    }
+
+    if (!_fndefs.empty()) {
+        // Function declarations
+        emitFuncDecls(oss, mangler, _fndefs);
+        oss << '\n';
+
+        // Function implementations
+        emitFuncImpls(oss, mangler, _fndefs);
+        oss << '\n';
+    }
+
+    // Statements
+    for (size_t i = 0; i < _nodes.size(); ++i) {
+        emitStmt(oss, mangler, _nodes[i]);
+        if (i != _nodes.size() - 1) {
+            oss << '\n';
+        }
+    }
+
+    return oss.str();
+}
+
+void LuaBlock::emitLocalVarDecls(std::ostringstream& oss,
+                                 LuaNameMangler& mangler,
+                                 const std::vector<Symbol*>& locals) {
+    for (size_t i = 0; i < locals.size(); ++i) {
+        oss << "local " << mangler.mangle(locals[i]);
+        if (i != locals.size() - 1) {
+            oss << '\n';
+        }
+    }
+}
+
+void LuaBlock::emitFuncDecls(std::ostringstream& oss,
+                             LuaNameMangler& mangler,
+                             const std::vector<std::pair<SemanticFunc*, LuaFuncDef*>>& fndefs) {
+    for (size_t i = 0; i < fndefs.size(); ++i) {
+        auto [func, _] = fndefs[i];
+        oss << "local " << mangler.mangle(func);
+        if (i != fndefs.size() - 1) {
+            oss << '\n';
+        }
+    }
+}
+
+void LuaBlock::emitFuncImpls(std::ostringstream& oss,
+                             LuaNameMangler& mangler,
+                             const std::vector<std::pair<SemanticFunc*, LuaFuncDef*>>& fndefs) {
+    for (size_t i = 0; i < fndefs.size(); ++i) {
+        auto [func, fndef] = fndefs[i];
+        oss << mangler.mangle(func) << " = " << fndef->emit(mangler);
+        if (i != fndefs.size() - 1) {
+            oss << '\n';
+        }
+    }
+}
+
+void LuaBlock::emitStmt(std::ostringstream& oss, LuaNameMangler& mangler, LuaNode* node) {
+    if (LuaExpr* expr = node->as<LuaExpr>()) {
+        oss << "_ = " << expr->emit(mangler);
+    } else if (LuaStmt* stmt = node->as<LuaStmt>()) {
+        oss << stmt->emit(mangler);
+    }
+    // Ignores other types of nodes
+}
+
 std::string LuaIntExpr::emit(LuaNameMangler& mangler) const {
     return std::to_string(_value);
 }
@@ -80,7 +169,31 @@ std::string LuaBoolExpr::emit(LuaNameMangler& mangler) const {
 std::string LuaStringExpr::emit(LuaNameMangler& mangler) const {
     std::string s;
     s += '"';
-    s += _value;
+    for (char c : _value) {
+        switch (c) {
+            case '\n':
+                s += "\\n";
+                break;
+            case '\t':
+                s += "\\t";
+                break;
+            case '\r':
+                s += "\\r";
+                break;
+            case '\\':
+                s += "\\\\";
+                break;
+            case '"':
+                s += "\\\"";
+                break;
+            case '\'':
+                s += "\\'";
+                break;
+            default:
+                s += c;
+                break;
+        }
+    }
     s += '"';
     return s;
 }
@@ -127,6 +240,85 @@ std::string LuaMemberAccessExpr::emit(LuaNameMangler& mangler) const {
         assert(false && "invalid `std::variant<Symbol*, SemanticFunc*>` value");
     }
     return oss.str();
+}
+
+std::string LuaBlockExpr::emit(LuaNameMangler& mangler) const {
+    std::ostringstream oss;
+
+    oss << "(function()\n";
+
+    if (!_body->_locals.empty()) {
+        // Local variable declarations
+        LuaBlock::emitLocalVarDecls(oss, mangler, _body->_locals);
+        oss << '\n';
+    }
+
+    if (!_body->_fndefs.empty()) {
+        // Function declarations
+        LuaBlock::emitFuncDecls(oss, mangler, _body->_fndefs);
+        oss << '\n';
+
+        // Function implementations
+        LuaBlock::emitFuncImpls(oss, mangler, _body->_fndefs);
+        oss << '\n';
+    }
+
+    // Statements
+    if (_body->_nodes.empty()) {
+        oss << "return nil";
+    } else {
+        for (size_t i = 0; i < _body->_nodes.size() - 1; ++i) {
+            LuaBlock::emitStmt(oss, mangler, _body->_nodes[i]);
+            oss << '\n';
+        }
+        if (LuaExpr* lastExpr = _body->_nodes.back()->as<LuaExpr>()) {
+            oss << "return " << lastExpr->emit(mangler);
+        } else if (LuaStmt* lastStmt = _body->_nodes.back()->as<LuaStmt>()) {
+            oss << lastStmt->emit(mangler) << '\n'
+                << "return " << LuaVoidExpr().emit(mangler);
+        }
+    }
+
+    oss << "\nend)()";
+
+    return oss.str();
+}
+
+std::string LuaAssignStmt::emit(LuaNameMangler& mangler) const {
+    std::ostringstream oss;
+    oss << _lhs->emit(mangler) << " = " << _rhs->emit(mangler);
+    return oss.str();
+}
+
+std::string LuaIfStmt::emit(LuaNameMangler& mangler) const {
+    std::ostringstream oss;
+    oss << "if " << _cond->emit(mangler) << " then\n"
+            << _thenBody->emit(mangler) << '\n'
+        << "else\n"
+            << _elseBody->emit(mangler) << '\n'
+        << "end";
+    return oss.str();
+}
+
+std::string LuaReturnStmt::emit(LuaNameMangler& mangler) const {
+    return "return " + _expr->emit(mangler);
+}
+
+std::string LuaWhileStmt::emit(LuaNameMangler& mangler) const {
+    std::ostringstream oss;
+    oss << "while " << _cond->emit(mangler) << " do\n"
+            << _body->emit(mangler) << '\n'
+            << "::continue::\n"
+        << "end";
+    return oss.str();
+}
+
+std::string LuaBreakStmt::emit(LuaNameMangler& mangler) const {
+    return "break";
+}
+
+std::string LuaContinueStmt::emit(LuaNameMangler& mangler) const {
+    return "goto continue";
 }
 
 } // Spark::FrontEnd
--- src/frontend/semantic/lua_ir.hpp
+++ src/frontend/semantic/lua_ir.hpp
@@ -1,6 +1,7 @@
 ﻿#pragma once
 
 #include <cstdint>
+#include <memory>
 #include <string>
 #include <string_view>
 #include <unordered_map>
@@ -42,17 +43,80 @@ public:
 
     [[nodiscard]]
     virtual std::string emit(LuaNameMangler& mangler) const = 0;
+
+    template <typename T>
+    [[nodiscard]]
+    bool is() const {
+        return dynamic_cast<const T*>(this) != nullptr;
+    }
+
+    template <typename T>
+    [[nodiscard]]
+    T* as() {
+        return dynamic_cast<T*>(this);
+    }
+
+    template <typename T>
+    [[nodiscard]]
+    const T* as() const {
+        return dynamic_cast<const T*>(this);
+    }
 };
 
-class LuaBlock {
+class LuaBlock;
 
-};
+class LuaFuncDef : public LuaNode {
+private:
+    SemanticFunc* _func;
+    std::vector<Symbol*> _params;
+    LuaBlock* _body;
 
-class LuaExpr : public LuaNode {
 public:
+    LuaFuncDef(SemanticFunc* func, std::vector<Symbol*> params, LuaBlock* body) noexcept
+        : _func(func), _params(std::move(params)), _body(body) { }
+
     [[nodiscard]]
     std::string emit(LuaNameMangler& mangler) const override;
 };
+
+class LuaBlock : public LuaNode {
+private:
+    std::vector<Symbol*> _locals;
+
+    std::vector<std::pair<SemanticFunc*, LuaFuncDef*>> _fndefs;
+
+    std::vector<LuaNode*> _nodes;
+
+public:
+    LuaBlock(std::vector<Symbol*> locals,
+             std::vector<std::pair<SemanticFunc*, LuaFuncDef*>> funcDefs,
+             std::vector<LuaNode*> nodes) noexcept
+        : _locals(std::move(locals)), _fndefs(std::move(funcDefs)), _nodes(std::move(nodes)) { }
+
+    [[nodiscard]]
+    std::string emit(LuaNameMangler& mangler) const override;
+
+private:
+    static void emitLocalVarDecls(std::ostringstream& oss,
+                                  LuaNameMangler& mangler,
+                                  const std::vector<Symbol*>& locals);
+
+    static void emitFuncDecls(std::ostringstream& oss,
+                              LuaNameMangler& mangler,
+                              const std::vector<std::pair<SemanticFunc*, LuaFuncDef*>>& fndefs);
+
+    static void emitFuncImpls(std::ostringstream& oss,
+                              LuaNameMangler& mangler,
+                              const std::vector<std::pair<SemanticFunc*, LuaFuncDef*>>& fndefs);
+
+    static void emitStmt(std::ostringstream& oss, LuaNameMangler& mangler, LuaNode* node);
+
+    friend class LuaBlockExpr;
+};
+
+class LuaExpr : public LuaNode { };
+
+class LuaStmt : public LuaNode { };
 
 class LuaIntExpr : public LuaExpr {
 private:
@@ -165,6 +229,102 @@ public:
 
     [[nodiscard]]
     std::string emit(LuaNameMangler& mangler) const override;
+};
+
+class LuaBlockExpr : public LuaExpr {
+private:
+    LuaBlock* _body;
+
+public:
+    explicit LuaBlockExpr(LuaBlock* body) noexcept :
+        _body(body) { }
+
+    [[nodiscard]]
+    std::string emit(LuaNameMangler& mangler) const override;
+};
+
+class LuaAssignStmt : public LuaStmt {
+private:
+    LuaExpr* _lhs;
+    LuaExpr* _rhs;
+
+public:
+    LuaAssignStmt(LuaExpr* lhs, LuaExpr* rhs) noexcept
+        : _lhs(lhs), _rhs(rhs) { }
+
+    [[nodiscard]]
+    std::string emit(LuaNameMangler& mangler) const override;
+};
+
+class LuaIfStmt : public LuaStmt {
+private:
+    LuaExpr* _cond;
+    LuaBlock* _thenBody;
+    LuaBlock* _elseBody;
+
+public:
+    LuaIfStmt(LuaExpr* cond, LuaBlock* thenBody, LuaBlock* elseBody) noexcept
+        : _cond(cond), _thenBody(thenBody), _elseBody(elseBody) { }
+
+    [[nodiscard]]
+    std::string emit(LuaNameMangler& mangler) const override;
+};
+
+class LuaReturnStmt : public LuaStmt {
+private:
+    LuaExpr* _expr;
+
+public:
+    explicit LuaReturnStmt(LuaExpr* expr) noexcept
+        : _expr(expr) { }
+
+    [[nodiscard]]
+    std::string emit(LuaNameMangler& mangler) const override;
+};
+
+class LuaWhileStmt : public LuaStmt {
+private:
+    LuaExpr* _cond;
+    LuaBlock* _body;
+
+public:
+    LuaWhileStmt(LuaExpr* cond, LuaBlock* body) noexcept
+        : _cond(cond), _body(body) { }
+
+    [[nodiscard]]
+    std::string emit(LuaNameMangler& mangler) const override;
+};
+
+class LuaBreakStmt : public LuaStmt {
+public:
+    LuaBreakStmt() noexcept = default;
+
+    [[nodiscard]]
+    std::string emit(LuaNameMangler& mangler) const override;
+};
+
+class LuaContinueStmt : public LuaStmt {
+public:
+    LuaContinueStmt() noexcept = default;
+
+    [[nodiscard]]
+    std::string emit(LuaNameMangler& mangler) const override;
+};
+
+
+
+class LuaNodeTable {
+private:
+    std::vector<std::unique_ptr<LuaNode>> _nodes;
+
+public:
+    template <typename T, typename... Args>
+    T* makeType(Args&&... args) {
+        std::unique_ptr<T> up = std::make_unique<T>(std::forward<Args>(args)...);
+        T* p = up.get();
+        _nodes.emplace_back(std::move(up));
+        return p;
+    }
 };
 
 } // Spark::FrontEnd
